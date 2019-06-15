3207714898aa844c3d2e3e6bdbe112cfc721ee37
varnholt <matthias.varnholt@gmail.com>
2019-06-15 12:41:18 +1000

    deleted old levels

--- data/level-demo/enemies.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="enemies" tilewidth="48" tileheight="48" tilecount="8" columns="8">
- <image source="tilesets/enemies.png" width="384" height="48"/>
-</tileset>
--- data/level-new/collision_tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="collision_tiles" tilewidth="24" tileheight="24" tilecount="256" columns="16">
- <image source="collision_tiles.png" width="384" height="384"/>
-</tileset>
