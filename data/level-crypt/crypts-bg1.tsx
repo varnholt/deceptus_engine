d5375d0d255779fe07117e90a5cb22c0da4fec58
varnholt <matthias.varnholt@gmail.com>
2019-06-15 19:10:47 +1000

    cleanup

--- data/level-crypt/crypts-bg1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="crypts" tilewidth="24" tileheight="24" tilecount="1024" columns="32">
- <image source="tilesets/crypts.png" width="768" height="768"/>
-</tileset>
