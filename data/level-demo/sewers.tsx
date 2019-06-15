ac84c5eb03ae8c9a9ae46da1b65fe06b32f0727e
varnholt <matthias.varnholt@gmail.com>
2019-06-15 12:41:18 +1000

    deleted old levels

--- data/level-demo/sewers.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="background" tilewidth="24" tileheight="24" tilecount="256" columns="16">
- <image source="tilesets/background.png" width="384" height="384"/>
-</tileset>
