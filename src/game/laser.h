ec327e3a50e9dfc9854a1c4b1aecaa9c617a91bb
varnholt <matthias.varnholt@gmail.com>
2019-06-16 22:34:39 +1000

    continued lasers

--- src/game/laser.h
+++ src/game/laser.h
@@ -17,6 +17,12 @@ class Laser
 {
 public:
 
+   struct Signal
+   {
+      int32_t mDurationMs = 0;
+      bool mOn = false;
+   };
+
    Laser() = default;
 
    void draw(sf::RenderTarget& window);
@@ -29,18 +35,20 @@ public:
       const std::shared_ptr<b2World>& world
    );
 
-   void addSprite(const sf::Sprite&);
-
    int getZ() const;
    void setZ(int z);
 
 
 protected:
 
+   std::vector<Signal> mSignalPlot;
+
    sf::Vector2u mTileSize;
    sf::Texture mTexture;
+   int32_t mTu = 0;
+   int32_t mTv = 0;
 
-   std::vector<sf::Sprite> mSprites;
+   sf::Sprite mSprite;
 
    sf::Vector2f mTilePosition;
 
