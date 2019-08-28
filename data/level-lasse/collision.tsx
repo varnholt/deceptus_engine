69aa41c56833006a3272bac34ed2928891172f5a
Matthias Varnholt <matthias.varnholt@seeingmachines.com>
2019-08-28 13:37:43 +1000

    added lasse birthday level

---
+++ data/level-lasse/collision.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="collision" tilewidth="24" tileheight="24" tilecount="1" columns="1">
+ <image source="tilesets/collision.png" width="24" height="24"/>
+</tileset>
