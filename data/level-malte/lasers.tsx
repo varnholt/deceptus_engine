eb6eb21f3016178f2290dd3e7c870b3a3dd768bf
Matthias Varnholt <matthias.varnholt@seeingmachines.com>
2019-10-14 16:09:24 +1100

    added level-malte

---
+++ data/level-malte/lasers.tsx
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="lasers" tilewidth="24" tileheight="24" tilecount="400" columns="20">
+ <image source="tilesets/lasers.png" width="480" height="481"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="5000"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="5000"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="0" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="80">
+  <animation>
+   <frame tileid="80" duration="5000"/>
+   <frame tileid="81" duration="100"/>
+   <frame tileid="82" duration="100"/>
+   <frame tileid="83" duration="100"/>
+   <frame tileid="84" duration="100"/>
+   <frame tileid="85" duration="100"/>
+   <frame tileid="86" duration="5000"/>
+   <frame tileid="85" duration="100"/>
+   <frame tileid="84" duration="100"/>
+   <frame tileid="83" duration="100"/>
+   <frame tileid="82" duration="100"/>
+   <frame tileid="81" duration="100"/>
+   <frame tileid="80" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="100">
+  <animation>
+   <frame tileid="100" duration="5000"/>
+   <frame tileid="101" duration="100"/>
+   <frame tileid="102" duration="100"/>
+   <frame tileid="103" duration="100"/>
+   <frame tileid="104" duration="100"/>
+   <frame tileid="105" duration="100"/>
+   <frame tileid="106" duration="5000"/>
+   <frame tileid="105" duration="100"/>
+   <frame tileid="104" duration="100"/>
+   <frame tileid="103" duration="100"/>
+   <frame tileid="102" duration="100"/>
+   <frame tileid="101" duration="100"/>
+   <frame tileid="100" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
