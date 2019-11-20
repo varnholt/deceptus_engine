7715ca1f111b0ca81554158e20ac02dcafa7d2e9
varnholt <matthias.varnholt@gmail.com>
2019-11-20 23:25:34 +1100

    added tristate to lever

--- src/game/lever.cpp
+++ src/game/lever.cpp
@@ -3,17 +3,53 @@
 
 void Lever::toggle()
 {
-   switch (mState)
+   if (mType == Type::TwoState)
    {
-      case State::Off:
-         mState = State::On;
-         break;
-      case State::On:
-         mState = State::Off;
-         break;
+      switch (mState)
+      {
+         case State::Left:
+            mState = State::Right;
+            break;
+         case State::Right:
+            mState = State::Left;
+            break;
+         case State::Middle:
+            break;
+      }
    }
 
-   mCallback(mState == State::Off);
+   else if (mType == Type::TriState)
+   {
+      switch (mState)
+      {
+         case State::Left:
+         {
+            mState = State::Middle;
+            break;
+         }
+         case State::Middle:
+         {
+            if (mPreviousState == State::Left)
+            {
+               mState = State::Right;
+            }
+            else
+            {
+               mState = State::Left;
+            }
+            break;
+         }
+         case State::Right:
+         {
+            mState = State::Middle;
+            break;
+         }
+      }
+
+      mPreviousState = mState;
+   }
+
+   mCallback(static_cast<int32_t>(mState));
 }
 
 
