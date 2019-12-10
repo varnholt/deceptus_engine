9d0e074333096b3bc4b93b02ab6f454a241ac1e1
varnholt <matthias.varnholt@gmail.com>
2019-12-10 21:23:19 +1100

    deploying to relative build dir

---
+++ deploy_appveyor.bat
@@ -0,0 +1,8 @@
+IF NOT EXIST build mkdir build
+IF NOT EXIST build\data mkdir build\data
+xcopy /Y lib64\*.dll build
+xcopy /Y sfml\bin\*.dll build
+xcopy /Y SDL\lib\x64\*.dll build
+xcopy /E /Y data build\data
+xcopy /Y deceptus.exe build
+IF EXIST deceptus.pdb xcopy /C /Y deceptus.pdb build
