13598a910e745dd8b701fe83723eeca24ef64302
varnholt <matthias.varnholt@gmail.com>
2020-01-20 23:44:25 +1100

    added bounce on heavy collision

--- src/game/boomeffect.h
+++ src/game/boomeffect.h
@@ -7,11 +7,16 @@ struct BoomEffect
 {
    void boom(float x, float y, float intensity = 1.0f);
    void update(const sf::Time& dt);
+   float getRemainingTime() const;
 
    sf::Time mBoomTimeEnd;
    float mBoomFactor = 1.0f;
    float mBoomOffsetX = 0.0f;
    float mBoomOffsetY = 0.0f;
    float mBoomDuration = 1.0f;
+   float mFactorX = 0.0f;
+   float mFactorY = 0.0f;
+   float mEffectVelocity = 32.0f;
+   float mEffectAmplitude = 0.1f;
 };
 
