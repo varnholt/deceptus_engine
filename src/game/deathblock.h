0ed774d7b6f9caab238e6303858ccffb0d98c40c
varnholt <matthias.varnholt@gmail.com>
2020-05-06 23:34:11 +1000

    added collision check to deathblocks

--- src/game/deathblock.h
+++ src/game/deathblock.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "constants.h"
 #include "gamemechanism.h"
 #include "gamenode.h"
 #include "pathinterpolation.h"
@@ -51,6 +52,7 @@ class DeathBlock : public GameMechanism, public GameNode
       void setupTransform();
       void setupBody(const std::shared_ptr<b2World>& world);
       void updateLeverLag(const sf::Time& dt);
+      void updateCollision();
 
       int32_t mTu = 0;
       int32_t mTv = 0;
@@ -64,7 +66,9 @@ class DeathBlock : public GameMechanism, public GameNode
       //     +---+
 
       std::array<sf::Sprite, 5> mSprites;
+
       std::array<int32_t, 5> mStates = {};
+
       std::array<sf::Vector2i, 5> mOffsets = {
          sf::Vector2i{1, 0},
          sf::Vector2i{0, 1},
@@ -73,10 +77,16 @@ class DeathBlock : public GameMechanism, public GameNode
          sf::Vector2i{2, 1}
       };
 
+      std::array<sf::IntRect, 4> mCollisionRects = {
+          sf::IntRect{1 * PIXELS_PER_TILE, 0 * PIXELS_PER_TILE, PIXELS_PER_TILE, PIXELS_PER_TILE},
+          sf::IntRect{0 * PIXELS_PER_TILE, 1 * PIXELS_PER_TILE, PIXELS_PER_TILE, PIXELS_PER_TILE},
+          sf::IntRect{1 * PIXELS_PER_TILE, 2 * PIXELS_PER_TILE, PIXELS_PER_TILE, PIXELS_PER_TILE},
+          sf::IntRect{2 * PIXELS_PER_TILE, 1 * PIXELS_PER_TILE, PIXELS_PER_TILE, PIXELS_PER_TILE}
+      };
+
       int32_t mElapsedMs = 0;
 
       sf::Vector2f mPixelPosition;
-      sf::IntRect mPixelRect;
       std::vector<sf::Vector2f> mPixelPath;
 
       b2Body* mBody = nullptr;
