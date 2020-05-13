0fdf2fe3705604e45f7d308b57ff2405b4163217
varnholt <matthias.varnholt@gmail.com>
2020-05-13 22:54:52 +1000

    critter is almost done

--- data/scripts/enemies/critter.lua
+++ data/scripts/enemies/critter.lua
@@ -46,11 +46,15 @@ properties = {
 
 ------------------------------------------------------------------------------------------------------------------------
 mPosition = v2d.Vector2D(0, 0)
-mCenter = v2d.Vector2D(0, 0)
+mOffset = v2d.Vector2D(0, 0)
 mElapsed = 0.0
 mSpriteIndex = 0
 mPointsLeft = false
 mLoop = false
+mSpeed = 1.0
+
+EPSILON = 0.0001
+DISTANCE_FROM_PATH_PX = 10
 
 
 ------------------------------------------------------------------------------------------------------------------------
@@ -64,14 +68,19 @@ end
 
 
 ------------------------------------------------------------------------------------------------------------------------
+
+-- cw
 -- 0: right, horizontal, up
--- 1: left, horizontal, up
--- 2: right, horizontal, down
 -- 3: left, horizontal, down
 -- 4: right, vertical, down
+-- 7: left, vertical, up
+
+-- ccw
+-- 2: right, horizontal, down
+-- 1: left, horizontal, up
 -- 5: right, vertical, up
 -- 6: left, vertical, down
--- 7: left, vertical, up
+
 --
 -- clockwise
 -- 0 -> 4 -> 3 -> 7
@@ -89,18 +98,20 @@ function updateSprite(p)
 
    row = 0
 
-   epsilon = 0.001
-
-   if (dx > epsilon) then
+   if (dx > EPSILON) then
       row = 2
-   elseif  (dx < -epsilon) then
+      -- mOffset = v2d.Vector2D(0, DISTANCE_FROM_PATH_PX)
+   elseif  (dx < -EPSILON) then
       row = 1
+      -- mOffset = v2d.Vector2D(0, -DISTANCE_FROM_PATH_PX)
    end
 
-   if  (dy > epsilon) then
+   if (dy > EPSILON) then
       row = 6
-   elseif (dy < -epsilon) then
+      -- mOffset = v2d.Vector2D(-DISTANCE_FROM_PATH_PX, 0)
+   elseif (dy < -EPSILON) then
       row = 5
+      -- mOffset = v2d.Vector2D(DISTANCE_FROM_PATH_PX, 0)
    end
 
    updateSpriteRect(0, row * 48, 48, 48)
@@ -116,11 +127,11 @@ function update(dt)
 
    mElapsed = mElapsed + dt
 
-   p = getValueLinear(mPatrolPath, math.fmod(mElapsed * 0.25, 1.0));
+   p = getValueLinear(mPatrolPath, math.fmod(mSpeed * mElapsed * 0.2, 1.0));
 
    setTransform(
-      p:getX(),
-      p:getY(),
+      p:getX() + mOffset:getX(),
+      p:getY() + mOffset:getY(),
       0.0
    )
 
@@ -156,22 +167,65 @@ function setPath(name, table)
 
    if (name == "patrol_path") then
 
+      -- need to scale each path, so it increases in 24px width and 24px height
+      minX = 9999
+      maxX = 0
+      minY = 9999
+      maxY = 0
+
+      for key, value in pairs(path) do
+         x = value:getX()
+         y = value:getY()
+
+         minX = math.min(minX, x)
+         maxX = math.max(maxX, x)
+         minY = math.min(minY, y)
+         maxY = math.max(maxY, y)
+      end
+
+      width = maxX - minX
+      height = maxY - minY
+      factorX = (width + 24.0) / width
+      factorY = (height + 24.0) / height
+
+      print(string.format("width: %f, height: %f, factorX: %f, factorY: %f", width, height, factorX, factorY))
+
+      for key, value in pairs(path) do
+         value:setX(minX + (value:getX() - minX) * factorX - 12)
+         value:setY(minY + (value:getY() - minY) * factorY - 12)
+      end
+
+      -- generate patrol path
+      length = 0.0
+      posPrev = path[1]
+
       -- create loop and leave space for one extra item at the end
       i = 1
       for key, value in pairs(path) do
          mPatrolPath[i] = Key:create{x = value:getX(), y = value:getY(), time = (i - 1) / (#path + 1)}
          i = i + 1
+
+         -- append to length
+         length = length + (value - posPrev):getLength()
+         posPrev = value
       end
 
       -- close the loop
       value = path[1]
       mPatrolPath[i]  = Key:create{x = value:getX(), y = value:getY(), time = 1.0}
+      length = length + (value - posPrev):getLength()
    end
 
+   -- the length of 3 tiles next to each other is the default length for the speed computation
+   mSpeed = 192 / length
+
+
+   -- print(string.format("length: %f, speed: %f", length, mSpeed))
+
    -- debug the loop
-   for key, value in pairs(mPatrolPath) do
-      print(string.format("patrolpath: %d: %f, %f, time: %f", key, value.x, value.y, value.time))
-   end
+   -- for key, value in pairs(mPatrolPath) do
+   --   print(string.format("patrolpath: %d: %f, %f, time: %f", key, value.x, value.y, value.time))
+   -- end
 end
 
 
@@ -183,6 +237,6 @@ end
 
 ------------------------------------------------------------------------------------------------------------------------
 function movedTo(x, y)
-   mPosition = v2d.Vector2D(x, y)
+   -- mPosition = v2d.Vector2D(x, y)
 end
 
