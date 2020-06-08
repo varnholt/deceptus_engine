6a0b543b99126a66de2b2640808f47b65b13720c
varnholt <matthias.varnholt@gmail.com>
2020-06-08 21:34:19 +1000

    prepared smoke effect

---
+++ src/effects/smokeeffect.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "effect.h"
+
+#include <SFML/Graphics.hpp>
+
+#include <array>
+#include <memory>
+#include <vector>
+
+
+struct TmxObject;
+struct TmxObjectGroup;
+
+class SmokeEffect : public Effect
+{
+
+public:
+
+   static const std::string sLayerName;
+
+   struct SmokeParticle
+   {
+      sf::Sprite mSprite;
+      float mRot = 0.0f;
+      float mTimeOffset = 0.0f;
+   };
+
+   std::array<SmokeParticle, 30> mParticles;
+
+   static std::shared_ptr<SmokeEffect> deserialize(TmxObject* tmxObject, TmxObjectGroup* objectGroup);
+
+
+public:
+
+   SmokeEffect();
+
+   static sf::Texture mTexture;
+
+   int mZ = 0;
+
+   void drawToZ(sf::RenderTarget& target, sf::RenderStates states, int z) const;
+   void onDraw(sf::RenderTarget& target, sf::RenderStates states) const override;
+   void onUpdate(const sf::Time& time, float x, float y) override;
+   bool onLoad() override;
+};
+
