4bd6568035ed4008a359cfc765c06d38dbbeb77a
varnholt <matthias.varnholt@gmail.com>
2020-06-10 23:34:52 +1000

    continued smoke effect

--- src/effects/effect.h
+++ src/effects/effect.h
@@ -9,32 +9,23 @@ class Effect : public sf::Drawable
 {
 
 public:
-
   virtual ~Effect() override = default;
-
   const std::string& getName() const;
-
   void load();
-
   void update(const sf::Time& time, float x, float y);
-
   void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
-
 protected:
-
   Effect(const std::string& name);
-
 
 private:
 
-  // Virtual functions to be implemented in derived effects
   virtual bool onLoad() = 0;
   virtual void onUpdate(const sf::Time& time, float x, float y) = 0;
   virtual void onDraw(sf::RenderTarget& target, sf::RenderStates states) const = 0;
 
 
-private:
+protected:
 
   std::string mName;
   bool mIsLoaded = false;
