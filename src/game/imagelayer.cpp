289f0ff36562b03823ec39e5932aeab9809bb35d
varnholt <matthias.varnholt@gmail.com>
2020-06-10 22:02:19 +1000

    be able to apply atmosphere shader to image layer

--- src/game/imagelayer.cpp
+++ src/game/imagelayer.cpp
@@ -5,12 +5,15 @@
 #include "tmxparser/tmxproperties.h"
 #include "tmxparser/tmxproperty.h"
 
+#include <iostream>
+
 
 //-----------------------------------------------------------------------------
 std::shared_ptr<ImageLayer> ImageLayer::deserialize(TmxElement* element, const std::filesystem::path& levelPath)
 {
   std::shared_ptr<ImageLayer> image = std::make_shared<ImageLayer>();
   auto imageLayer = dynamic_cast<TmxImageLayer*>(element);
+
   image->mZ = imageLayer->mZ;
   image->mTexture.loadFromFile((levelPath / imageLayer->mImage->mSource).string());
   image->mSprite.setPosition(imageLayer->mOffsetX, imageLayer->mOffsetY);
@@ -20,6 +23,13 @@ std::shared_ptr<ImageLayer> ImageLayer::deserialize(TmxElement* element, const s
   sf::BlendMode blendMode = sf::BlendAdd;
   if (imageLayer->mProperties != nullptr)
   {
+     auto z = imageLayer->mProperties->mMap.find("z");
+     if (z != imageLayer->mProperties->mMap.end())
+     {
+        image->mZ = imageLayer->mProperties->mMap["z"]->mValueInt;
+        std::cout << "image layer has z: " << image->mZ << std::endl;
+     }
+
      std::string blendModeStr;
      auto it = imageLayer->mProperties->mMap.find("blendmode");
      if (it != imageLayer->mProperties->mMap.end())
