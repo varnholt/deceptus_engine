fa1d363c9d3398b2cd5b93428e88ac5f3b77a64c
varnholt <matthias.varnholt@gmail.com>
2020-06-25 20:51:58 +1000

    added missing steps

--- lab/generate_ao/main.cpp
+++ lab/generate_ao/main.cpp
@@ -13,16 +13,37 @@
 namespace
 {
    constexpr auto blurRadius = 8;
+   static const auto black = qRgba(0, 0, 0, 128);
+   static const auto transparent = qRgba(0, 0, 0, 0);
 }
 
 
 QImage blurImage(const QImage& inputImage)
 {
+   // create black/transparent texture which is blurred
+   std::cout << "[x] creating black/transparent texture" << std::endl;
+
+   QImage blackWhite(inputImage);
+   for (auto x = 0; x < blackWhite.width(); x++)
+   {
+      for (auto y = 0; y < blackWhite.height(); y++)
+      {
+         const auto px = inputImage.pixel(x, y);
+         if (qAlpha(px) != 0)
+         {
+            blackWhite.setPixel(x, y, black);
+         }
+      }
+   }
+
+   // blur the thing
+   std::cout << "[x] blurring black/transparent texture" << std::endl;
+
    auto blur = new QGraphicsBlurEffect();
    blur->setBlurRadius(blurRadius);
 
    auto item = new QGraphicsPixmapItem();
-   item->setPixmap(QPixmap::fromImage(inputImage));
+   item->setPixmap(QPixmap::fromImage(blackWhite));
    item->setGraphicsEffect(blur);
 
    QGraphicsScene scene;
@@ -43,6 +64,20 @@ QImage blurImage(const QImage& inputImage)
          static_cast<double>(inputImage.height())
       }
    );
+
+   // cut out original contents
+   std::cout << "[x] cutting out original contents... " << std::endl;
+   for (auto x = 0; x < outputImage.width(); x++)
+   {
+      for (auto y = 0; y < outputImage.height(); y++)
+      {
+         const auto px = inputImage.pixel(x, y);
+         if (qAlpha(px) != 0)
+         {
+            outputImage.setPixel(x, y, transparent);
+         }
+      }
+   }
 
    return outputImage;
 }
@@ -76,8 +111,12 @@ int32_t main(int32_t argc, char* argv[])
    }
 
    auto outputFilename = QString("%1_ao.%2").arg(fileInfo.baseName()).arg(fileInfo.suffix());
+
+   std::cout << "[x] loading original texture" << std::endl;
    const QImage source(fileInfo.filePath());
    const QImage result = blurImage(source);
+
+   std::cout << "[x] writing texture to disk" << std::endl;
    result.save(outputFilename);
    std::cout << "[x] written ao texture to: " << outputFilename.toStdString() << std::endl;
 
