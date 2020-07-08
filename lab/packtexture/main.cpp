1407557bda53d37fe155da31feccfafab79a0131
varnholt <matthias.varnholt@gmail.com>
2020-07-08 14:07:16 +1000

    added command line support for texture packer

--- lab/packtexture/main.cpp
+++ lab/packtexture/main.cpp
@@ -1,12 +1,74 @@
 #include "mainwindow.h"
+#include "packtexture.h"
+
+#include <iostream>
 #include <QApplication>
+#include <QCommandLineParser>
 
 int main(int argc, char *argv[])
 {
-  QApplication a(argc, argv);
-  MainWindow w;
-  w.show();
-  w.setFixedSize(550,550);
+   QApplication a(argc, argv);
 
-  return a.exec();
+   QCoreApplication::setApplicationName("packtexture");
+   QCoreApplication::setApplicationVersion("1.0");
+
+   QCommandLineParser parser;
+   parser.addHelpOption();
+   parser.addVersionOption();
+
+   QCommandLineOption inputOption(
+      QStringList() << "i" << "input",
+      QCoreApplication::translate("main", "specify input file"),
+      QCoreApplication::translate("main", "file path")
+   );
+
+   QCommandLineOption sizeOption(
+      QStringList() << "s" << "size",
+      QCoreApplication::translate("main", "specify quad size, available values: 16, 24, 32, 64, 128, 256, 512, 1024"),
+      QCoreApplication::translate("main", "quad size")
+   );
+
+   parser.addOption(inputOption);
+   parser.addOption(sizeOption);
+   parser.process(a);
+
+   if (parser.isSet(inputOption))
+   {
+      PackTexture pt;
+      int32_t size = pt.size_;
+
+      if (parser.isSet(sizeOption))
+      {
+         bool ok = false;
+         size = parser.value(sizeOption).toInt(&ok);
+
+         if (!ok)
+         {
+            std::cerr << "bad value for size parameter" << std::endl;
+            parser.showHelp(1);
+         }
+
+         auto supportedValues = {16, 24, 32, 64, 128, 256, 512, 1024};
+
+         auto it = std::find(supportedValues.begin(), supportedValues.end(), size);
+         if (it == supportedValues.end())
+         {
+            std::cerr << "bad value for size parameter" << std::endl;
+            parser.showHelp(2);
+         }
+      }
+
+      pt.size_ = size;
+      pt.load(parser.value(inputOption));
+      pt.pack();
+      pt.dump();
+
+      return 0;
+   }
+
+   MainWindow w;
+   w.show();
+   w.setFixedSize(550,550);
+
+   return a.exec();
 }
--- lab/packtexture/texturelabel.cpp
+++ lab/packtexture/texturelabel.cpp
@@ -5,30 +5,29 @@
 TextureLabel::TextureLabel(QWidget* p)
  : QLabel(p)
 {
-
 }
 
 
 void TextureLabel::paintEvent(QPaintEvent * /*e*/)
 {
-  QPainter p(this);
+   QPainter p(this);
 
-  if (!pixmap_.isNull())
-  {
-    p.drawPixmap(0, 0, pixmap_);
-  }
+   if (!pixmap_.isNull())
+   {
+      p.drawPixmap(0, 0, pixmap_);
+   }
 
-  if (quads_ != nullptr)
-  {
-    p.setBrush(QColor(255,0,0,60));
-    std::for_each(quads_->begin(), quads_->end(), [&](Quad& q)
-    {
-      p.drawRect(
-        q.x_ * scaleX_,
-        q.y_ * scaleY_,
-        q.w_ * scaleX_,
-        q.h_ * scaleY_
-      );
-    });
-  }
+   if (quads_ != nullptr)
+   {
+      p.setBrush(QColor(255,0,0,60));
+      std::for_each(quads_->begin(), quads_->end(), [&](Quad& q)
+      {
+         p.drawRect(
+           q.x_ * scaleX_,
+           q.y_ * scaleY_,
+           q.w_ * scaleX_,
+           q.h_ * scaleY_
+         );
+      });
+   }
 }
