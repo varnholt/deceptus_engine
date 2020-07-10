eafee2fb5f94f0a18081fef61c137ba5f58b4aaa
varnholt <matthias.varnholt@gmail.com>
2020-07-10 22:14:31 +1000

    fixed packtexture alpha channel, fixed alpha texture for malte level

--- lab/packtexture/packtexture.cpp
+++ lab/packtexture/packtexture.cpp
@@ -8,16 +8,16 @@
 
 bool PackTexture::load(const QString& filename)
 {
-   filename_ = filename;
-   return image_.load(filename_);
+   mFilename = filename;
+   return mImage.load(mFilename);
 }
 
 
 void PackTexture::pack()
 {
-   quads_.clear();
-   auto sizeX = static_cast<int>(ceil(image_.width() / static_cast<float>(size_)));
-   auto sizeY = static_cast<int>(ceil(image_.height() / static_cast<float>(size_)));
+   mQuads.clear();
+   auto sizeX = static_cast<int>(ceil(mImage.width() / static_cast<float>(mSize)));
+   auto sizeY = static_cast<int>(ceil(mImage.height() / static_cast<float>(mSize)));
    auto progress = 0;
    auto progressDone = sizeX * sizeY;
 
@@ -25,24 +25,24 @@ void PackTexture::pack()
    {
       for (auto blockX = 0; blockX < sizeX; blockX++)
       {
-         if (updateProgress_)
+         if (mUpdateProgress)
          {
-            updateProgress_(100 * static_cast<int32_t>((static_cast<float>(progress)/progressDone)));
+            mUpdateProgress(100 * static_cast<int32_t>((static_cast<float>(progress)/progressDone)));
          }
 
          progress++;
 
-         QImage subImage = image_.copy(
-           blockX * size_,
-           blockY * size_,
-           size_,
-           size_
+         QImage subImage = mImage.copy(
+           blockX * mSize,
+           blockY * mSize,
+           mSize,
+           mSize
          );
 
          auto valid = false;
-         for (auto yi = 0; yi < size_; yi++)
+         for (auto yi = 0; yi < mSize; yi++)
          {
-            for (auto xi = 0; xi < size_; xi++)
+            for (auto xi = 0; xi < mSize; xi++)
             {
                auto rgb = subImage.pixel(xi, yi);
                if (qAlpha(rgb) != 0)
@@ -56,80 +56,81 @@ void PackTexture::pack()
          if (valid)
          {
             Quad q;
-            q.data_ = subImage;
-            q.x_ = blockX * size_;
-            q.y_ = blockY * size_;
-            q.w_ = size_;
-            q.h_ = size_;
-            quads_.push_back(q);
+            q.mData = subImage;
+            q.mX = blockX * mSize;
+            q.mY = blockY * mSize;
+            q.mW = mSize;
+            q.mH = mSize;
+            mQuads.push_back(q);
          }
       }
    }
 
-   if (updateProgress_)
+   if (mUpdateProgress)
    {
-      updateProgress_(100);
+      mUpdateProgress(100);
    }
 }
 
 
 void PackTexture::dump()
 {
-   auto count = quads_.size();
+   auto count = mQuads.size();
 
    std::cout << "[x] quad count: " << count << std::endl;
    std::array<int, 5> textureSizes = {512, 1024, 2048, 4096, 8192};
 
    auto suitable = std::find_if(std::begin(textureSizes), std::end(textureSizes), [&](auto textureSize){
-      auto tmp = textureSize / size_;
+      auto tmp = textureSize / mSize;
       tmp *= tmp;
       return (tmp >= count);
    });
 
-   textureSize_ = *suitable;
+   mTextureSize = *suitable;
 
-   std::cout << "[x] picking a " << textureSize_ << "x" << textureSize_ << " texture" << std::endl;
+   std::cout << "[x] picking a " << mTextureSize << "x" << mTextureSize << " texture" << std::endl;
 
-   QImage out(textureSize_, textureSize_, image_.format());
+   QImage out(mTextureSize, mTextureSize, mImage.format());
    QPainter painter(&out);
+   painter.setCompositionMode(QPainter::CompositionMode_Source);
 
-   const auto uvFilename = QString("%1_tiles.uv").arg(QFileInfo(filename_).baseName()).toStdString();
+   const auto uvFilename = QString("%1_tiles.uv").arg(QFileInfo(mFilename).baseName()).toStdString();
    std::cout << "[x] dumping uvs to: " << uvFilename << std::endl;
    std::ofstream uvFile(uvFilename);
    auto x = 0;
    auto y = 0;
    auto progress = 0;
 
-   for (auto& quad : quads_)
+   for (auto& quad : mQuads)
    {
       QPoint pos(x, y);
-      painter.drawImage(pos, quad.data_);
-      x += size_;
+      painter.drawImage(pos, quad.mData);
+      x += mSize;
 
       if (x == *suitable)
       {
          x = 0;
-         y += size_;
+         y += mSize;
       }
 
-      if (updateProgress_)
+      if (mUpdateProgress)
       {
-         updateProgress_(100 * static_cast<int32_t>((static_cast<float>(progress)/quads_.size())));
+         mUpdateProgress(100 * static_cast<int32_t>((static_cast<float>(progress)/mQuads.size())));
       }
 
-      uvFile << progress << ";" << quad.x_ << ";" << quad.y_ << ";" << quad.w_ << ";" << quad.h_ << std::endl;
+      uvFile << progress << ";" << quad.mX << ";" << quad.mY << ";" << quad.mW << ";" << quad.mH << std::endl;
       progress++;
    }
 
    painter.end();
 
-   const auto textureFilename = QString("%1_tiles.png").arg(QFileInfo(filename_).baseName());
+   const auto textureFilename = QString("%1_tiles.png").arg(QFileInfo(mFilename).baseName());
    std::cout << "[x] dumping texture to: " << textureFilename.toStdString() << std::endl;
    out.save(textureFilename);
 
-   if (updateProgress_)
+   if (mUpdateProgress)
    {
-      updateProgress_(100);
+      mUpdateProgress(100);
    }
 
    std::cout << "[x] done!" << std::endl;
