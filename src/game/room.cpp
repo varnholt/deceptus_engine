8ea87522b3a9c3a90697f24333cc46137d15b8f0
varnholt <matthias.varnholt@gmail.com>
2020-08-15 00:28:28 +0200

    continued room concept

--- src/game/room.cpp
+++ src/game/room.cpp
@@ -1,7 +1,7 @@
 #include "room.h"
 
 #include <algorithm>
-
+#include "gameconfiguration.h"
 
 /*
 
@@ -41,33 +41,73 @@ Room::Room(const std::vector<sf::IntRect>& rect)
 }
 
 
-bool Room::contains(const sf::Vector2i& p) const
+std::vector<sf::IntRect>::const_iterator Room::findRect(const sf::Vector2i& p) const
 {
    const auto it = std::find_if(mRects.begin(),mRects.end(), [p](const sf::IntRect& rect){return rect.contains(p);});
-   return it != mRects.end();
+   return it;
 }
 
 
-void Room::correctCamera(const sf::Vector2i& /*cameraCenter*/, const Room& /*activeRoom*/)
+std::optional<sf::Vector2i> Room::correctedCamera(const sf::Vector2i& cameraCenter, const Room& /*activeRoom*/)
 {
    // workflow (only for 'current' room)
    //
-   // 1) check which in which rectangles the current camera center lies
-   //    -> build a vector of intrects
-   //
-   //    well, ideally rooms shouldn't overlap but it might be worthwhile to
-   //    support it anyway
-   //
-   // 2) go through each intrect and calculate the room for the camera center, i.e.
-   //    - the minimum x (left)
-   //    - the maximum x (right)
-   //    - ...
-   //
-   // 3) check if
+   // 1) check which in which rectangle the current camera center lies
+   //    -> find the right intrect
+   const auto rectIt =  findRect(cameraCenter);
+   if (rectIt == mRects.end())
+   {
+      // that's an error.
+      return {};
+   }
+
+   // 2) check if
    //    a) screen's right is within room bounds, assign if necessary
    //    b) screen's left is within room bounds, assign if necessary
    //    c) screen's top is within room bounds, assign if necessary
    //    d) screen's bottom is within room bounds, assign if necessary
+
+   const auto rect = *rectIt;
+
+   const auto config = GameConfiguration::getInstance();
+   const auto halfWidth = config.mViewWidth / 2;
+   const auto halfHeight = config.mViewHeight / 2;
+
+   const auto l = cameraCenter + sf::Vector2i{- halfWidth, 0};
+   const auto r = cameraCenter + sf::Vector2i{  halfWidth, 0};
+   const auto u = cameraCenter + sf::Vector2i{0, - halfHeight};
+   const auto d = cameraCenter + sf::Vector2i{0,   halfHeight};
+
+   bool corrected = false;
+   std::optional<sf::Vector2i> correctedRect = cameraCenter;
+
+   if (!rect.contains(l))
+   {
+      // camera center is out of left boundary
+      correctedRect->x = l.x + halfWidth;
+      corrected = true;
+   }
+   else if (!rect.contains(r))
+   {
+      // camera center is out of right boundary
+      correctedRect->x = r.x - halfWidth;
+      corrected = true;
+   }
+
+   if (!rect.contains(u))
+   {
+      // camera center is out of upper boundary
+      correctedRect->y = u.y + halfHeight;
+      corrected = true;
+   }
+   else if (!rect.contains(d))
+   {
+      // camera center is out of lower boundary
+      correctedRect->y = d.y - halfHeight;
+      corrected = true;
+   }
+
+   return corrected ? correctedRect : std::optional<sf::Vector2i>{};
 }
 
 
