98c9393441b1c255a3830d4b5e9f6e60bbeaca5e
varnholt <matthias.varnholt@gmail.com>
2020-08-16 17:55:05 +0200

    added deserialiser code

--- src/game/room.h
+++ src/game/room.h
@@ -1,17 +1,27 @@
 #pragma once
 
 #include <optional>
+#include <map>
+#include <string>
 #include <vector>
+
 #include <SFML/Graphics.hpp>
+
+#include "tmxparser/tmxobject.h"
 
 struct Room
 {
    Room() = default;
+   Room(const sf::IntRect& rect);
    Room(const std::vector<sf::IntRect>& rect);
+
+   static void deserialize(TmxObject* tmxObject, std::vector<Room>& rooms);
+   static std::optional<Room> find(const sf::Vector2i& p, const std::vector<Room>& rooms);
 
    std::vector<sf::IntRect>::const_iterator findRect(const sf::Vector2i& p) const;
    std::optional<sf::Vector2i> correctedCamera(const sf::Vector2i& cameraCenter, const Room& activeRoom);
 
    std::vector<sf::IntRect> mRects;
+   std::string mName;
 };
 
