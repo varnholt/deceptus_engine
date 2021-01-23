f022c4da5eb2944349383fd1c1d27cea7834e0b2
varnholt <matthias.varnholt@gmail.com>
2021-01-23 23:17:24 +0100

    klonk 2 is on a good way

--- data/scripts/enemies/klonk_2.lua
+++ data/scripts/enemies/klonk_2.lua
@@ -13,17 +13,22 @@ properties = {
 ------------------------------------------------------------------------------------------------------------------------
 mPosition = v2d.Vector2D(0, 0)
 mPlayerPosition = v2d.Vector2D(0, 0)
-mSpriteIndex = 0
+mSpriteIndexStone = 0
+mSpriteIndexSmoke = 0
 mElapsed = 0.0
 mCycle = 0
 mSpeed = 35.0
+mRandomizedOffset = math.random(100)/100.0
 
+COLLISION_THRESHOLD = 24
 SPRITE_SIZE_PX = 4 * 24
 CYCLE_IDLE = 0
 CYCLE_WAKE = 1
 CYCLE_STEADY = 2
 CYCLE_FALLING = 3
 CYCLE_COLLIDE = 4
+CYCLE_DUST_BOTTOM_LEFT = 10
+CYCLE_DUST_BOTTOM_RIGHT = 11
 TILE_COUNT_IDLE = 3
 TILE_COUNT_WAKE = 12
 TILE_COUNT_STEADY = 12
@@ -38,7 +43,13 @@ function initialize()
    box_size_origin = 0.5 - box_size
 
    addShapeRect(0.48, 0.48, box_size_origin, box_size_origin)
+   addSprite() -- for dust left
+   addSprite() -- for dust right
    updateSpriteRect(0, 0, 0, SPRITE_SIZE_PX, SPRITE_SIZE_PX) -- id, x, y, width, height
+   updateSpriteRect(1, 0, 0, 1, 1)
+   updateSpriteRect(2, 0, 0, 1, 1)
+   setSpriteOffset(1, -38, -21)
+   setSpriteOffset(2, 38, -21)
 end
 
 
@@ -47,7 +58,21 @@ function update(dt)
 
    -- make sure block is on same x as player
    if (mCycle == CYCLE_IDLE) then
-      if (mPosition:getX() // 24 == mPlayerPosition:getX() // 24) then
+
+      mElapsed = mElapsed + dt
+      mSpriteIndexStone = math.floor(math.fmod(mRandomizedOffset + mElapsed, 3))
+
+      updateSpriteRect(
+         0,
+         mSpriteIndexStone * SPRITE_SIZE_PX,
+         CYCLE_IDLE * SPRITE_SIZE_PX,
+         SPRITE_SIZE_PX,
+         SPRITE_SIZE_PX
+      )
+
+      dx = mPosition:getX() - mPlayerPosition:getX()
+
+      if (dx > -COLLISION_THRESHOLD and dx < COLLISION_THRESHOLD) then
 
          -- make sure stone is not too far away (10 tiles) and above player
          yDiff = mPosition:getY() // 24 - mPlayerPosition:getY() // 24
@@ -65,6 +90,7 @@ function update(dt)
             )
             then
                -- activate the stone
+               mElapsed = 0.0
                mCycle = CYCLE_WAKE
             end
          end
@@ -75,13 +101,13 @@ function update(dt)
    if (mCycle == CYCLE_WAKE) then
 
       mElapsed = mElapsed + dt
-      mSpriteIndex = math.floor(mElapsed * mSpeed)
+      mSpriteIndexStone = math.floor(mElapsed * mSpeed)
 
       -- maybe close eyes again if player is out of sight?
-      if (mSpriteIndex < 12) then
+      if (mSpriteIndexStone < 12) then
          updateSpriteRect(
             0,
-            mSpriteIndex * SPRITE_SIZE_PX, 2 * SPRITE_SIZE_PX,
+            mSpriteIndexStone * SPRITE_SIZE_PX, 2 * SPRITE_SIZE_PX,
             SPRITE_SIZE_PX,
             SPRITE_SIZE_PX
          )
@@ -118,14 +144,28 @@ function update(dt)
    if (mCycle == CYCLE_COLLIDE) then
 
        mElapsed = mElapsed + dt
-       mSpriteIndex = math.floor(mElapsed * mSpeed)
+       mSpriteIndexStone = math.floor(mElapsed * mSpeed)
+       mSpriteIndexSmoke = math.floor(mElapsed * mSpeed)
 
-       if (mSpriteIndex < 7) then
-          updateSpriteRect(0, mSpriteIndex * SPRITE_SIZE_PX, 5 * SPRITE_SIZE_PX, SPRITE_SIZE_PX, SPRITE_SIZE_PX)
-        else
-          -- done? go back up?
-          mCycle = 5
+       if (mSpriteIndexStone < TILE_COUNT_COLLIDE) then
+          updateSpriteRect(0, mSpriteIndexStone * SPRITE_SIZE_PX, CYCLE_COLLIDE * SPRITE_SIZE_PX, SPRITE_SIZE_PX, SPRITE_SIZE_PX)
        end
+
+      updateSpriteRect(
+         1,
+         mSpriteIndexSmoke * SPRITE_SIZE_PX,
+         CYCLE_DUST_BOTTOM_LEFT * SPRITE_SIZE_PX,
+         SPRITE_SIZE_PX,
+         SPRITE_SIZE_PX
+      )
+
+      updateSpriteRect(
+         2,
+         mSpriteIndexSmoke * SPRITE_SIZE_PX,
+         CYCLE_DUST_BOTTOM_RIGHT * SPRITE_SIZE_PX,
+         SPRITE_SIZE_PX,
+         SPRITE_SIZE_PX
+      )
    end
 end
 
