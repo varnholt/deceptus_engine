6cb0b2f69f42cc890173eb8e6b22a7aea4e101ea
varnholt <matthias.varnholt@gmail.com>
2021-05-08 00:49:33 +0200

    started event serializer

---
+++ src/game/eventserializer.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include <chrono>
+#include <vector>
+
+class EventSerializer
+{
+   public:
+
+      struct ChronoEvent
+      {
+         std::chrono::time_point<std::chrono::steady_clock> _time_point;
+         sf::Event _event;
+      };
+
+      EventSerializer() = default;
+
+      void add(const sf::Event& event);
+      void clear();
+
+      void serialize();
+      void deserialize();
+
+
+   private:
+
+      std::vector<ChronoEvent> _events;
+};
+
