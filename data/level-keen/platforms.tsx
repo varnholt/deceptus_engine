6ce8373032d2ede159b3b3e187db51047020903f
varnholt <matthias.varnholt@gmail.com>
2021-07-22 23:16:07 +0200

    removed unused levels

--- data/level-keen/platforms.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="platforms" tilewidth="24" tileheight="24" tilecount="60" columns="10">
- <image source="tilesets/platforms.png" width="240" height="144"/>
-</tileset>
--- data/level-lasse/physics_tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="physics_tiles" tilewidth="24" tileheight="24" tilecount="256" columns="16">
- <image source="tilesets/collision_tiles.png" trans="ffffff" width="384" height="384"/>
-</tileset>
