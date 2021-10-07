aea1b016cc30062174c98fe49af8c9f8c2ac04eb
varnholt <matthias.varnholt@gmail.com>
2021-10-07 23:16:35 +0200

    backup

--- src/framework/tools/log.h
+++ src/framework/tools/log.h
@@ -10,7 +10,13 @@ void info(const std::string_view& message, const std::source_location& source = 
 void warning(const std::string_view& message, const std::source_location& source = std::source_location::current());
 void error(const std::string_view& message, const std::source_location& source = std::source_location::current());
 
-struct Info : public std::ostringstream{~Info();};
+struct Info : public std::ostringstream
+{
+   Info(const std::source_location& source_location = std::source_location::current());
+   ~Info();
+   const std::source_location& _source_location;
+};
+
 struct Warning : public std::ostringstream{~Warning();};
 struct Error : public std::ostringstream{~Error();};
 }
