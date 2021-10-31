069420a34216df04a0974fb5f007b42fbf6f7d83
varnholt <matthias.varnholt@gmail.com>
2021-10-31 18:44:34 +0100

    happy with platform drop

--- src/game/onewaywall.h
+++ src/game/onewaywall.h
@@ -2,12 +2,22 @@
 
 #include <Box2D/Box2D.h>
 
-namespace OneWayWall
+#include <set>
+
+
+struct OneWayWall
 {
+   static OneWayWall& instance();
 
-void beginContact(b2Contact* contact, b2Fixture* player_fixture, b2Fixture* platform_fixture);
-void endContact(b2Contact* contact);
-void drop();
+   void beginContact(b2Contact* contact, b2Fixture* player_fixture, b2Fixture* platform_fixture);
+   void endContact(b2Contact* contact);
+   void clear();
+   void drop();
+   bool hasContacts() const;
 
+private:
+
+   OneWayWall() = default;
+   std::set<b2Contact*> _contacts;
 };
 
