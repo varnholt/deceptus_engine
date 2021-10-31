70aa98b75b19e8c589dae81dcc2ca41198004cd0
varnholt <matthias.varnholt@gmail.com>
2021-10-31 02:50:59 +0200

    bumped cpan thresholds up a bit

--- src/game/tweaks.h
+++ src/game/tweaks.h
@@ -7,6 +7,8 @@ class Tweaks
       static const Tweaks& instance();
 
       float _bend_down_threshold = 0.6f;
+      float _cpan_tolerance_x = 0.2f;
+      float _cpan_tolerance_y = 0.2f;
 
    private:
       Tweaks() = default;
