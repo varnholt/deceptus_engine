14e5065a2cc93f91b74f1744a58d9f4ff64d2a30
varnholt <matthias.varnholt@gmail.com>
2021-11-07 22:47:36 +0100

    thunderstorm prototype

--- src/game/overlays/thunderstormoverlay.cpp
+++ src/game/overlays/thunderstormoverlay.cpp
@@ -1,17 +1,14 @@
 #include "thunderstormoverlay.h"
 
+#include "framework/math/fbm.h"
 #include "framework/tmxparser/tmxobject.h"
 
-
-ThunderstormOverlay::ThunderstormOverlay()
-{
-}
+#include <iostream>
 
 
 void ThunderstormOverlay::draw(sf::RenderTarget& target, sf::RenderTarget& /*normal*/)
 {
    const auto val = static_cast<uint8_t>(_value * 255);
-
    const auto col = sf::Color{val, val, val, val};
 
    sf::Vertex quad[] = {
@@ -22,31 +19,52 @@ void ThunderstormOverlay::draw(sf::RenderTarget& target, sf::RenderTarget& /*nor
    };
 
    sf::RenderStates states;
-//   states.shader = &_shader;
    states.blendMode = sf::BlendAlpha;
-
    target.draw(quad, 4, sf::Quads, states);
-
-
-   // _shape.setFillColor({val, val, val, val});
-   //
-   // target.draw(_shape);
 }
 
 
 void ThunderstormOverlay::update(const sf::Time& dt)
 {
    _time_s += dt.asSeconds();
-   _value = (sin(_time_s) + 1.0f) * 0.5f;
+
+   _value = fbm::fbm({_time_s, 0.0f}) * 3.0f;
+
+   if (_state == State::Lightning)
+   {
+      _thunderstorm_time_elapsed_s += dt.asSeconds();
+
+      _factor += dt.asSeconds() * 5.0f;;
+      _factor = std::min(_factor, 1.0f);
+      _value *= _factor;
+
+      if (_thunderstorm_time_elapsed_s > _settings._thunderstorm_time_min_s)
+      {
+         // start silence
+         _silence_time_elapsed_s = 0.0f;
+         _state = State::Silence;
+      }
+   }
+
+   if (_state == State::Silence)
+   {
+      _silence_time_elapsed_s += dt.asSeconds();
+
+      _factor -= dt.asSeconds() * 5.0f;;
+      _factor = std::max(_factor, 0.0f);
+      _value *= _factor;
+
+      if (_silence_time_elapsed_s > _settings._silence_time_s)
+      {
+         // start lightning
+         _thunderstorm_time_elapsed_s = 0.0f;
+         _state = State::Lightning;
+      }
+   }
 }
 
 
 void ThunderstormOverlay::setRect(const sf::FloatRect& rect)
 {
    _rect = rect;
-//   _shape.setSize({rect.width, rect.height});
-//   _shape.setPosition(rect.left, rect.top);
-
-//   _sprite.setPosition(rect.left, rect.top);
-//   _sprite.setScale();
 }
