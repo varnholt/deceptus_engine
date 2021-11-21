2a6921d308c85a198e824754fa326c340f52650f
varnholt <matthias.varnholt@gmail.com>
2021-11-21 21:51:49 +0100

    allow UV stretching in waterfall shader

--- src/game/shaderlayer.h
+++ src/game/shaderlayer.h
@@ -15,6 +15,9 @@ struct ShaderLayer
    sf::Vector2f _size;
    std::shared_ptr<sf::Texture> _texture;
    int32_t _z = 0;
+   float _time_offset = 0.0f;
+   float _uv_width = 1.0f;
+   float _uv_height = 1.0f;
 
    static std::shared_ptr<ShaderLayer> deserialize(TmxObject* element);
 };
