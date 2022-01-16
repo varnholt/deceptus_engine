ec0eda18b55b8d6c6b22e923df2862443a5b3836
varnholt <matthias.varnholt@gmail.com>
2022-01-16 01:22:41 +0100

    implemented log thread

--- src/framework/tools/log.h
+++ src/framework/tools/log.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <chrono>
 #include <functional>
 #include <source_location>
 #include <sstream>
@@ -7,7 +8,8 @@
 
 
 /*!
- * Log functions to output messages in a consistent manner including a timestamp and source location
+ * Log functions to output messages in a consistent manner including a timestamp and source location.
+ * In contrast to std::cout << "bla", a newline is always appended to each message.
  *
  * usages
  *    Log::info("Hello World");
@@ -15,6 +17,13 @@
  */
 namespace Log
 {
+
+enum class Level : char
+{
+   Info    = 'i',
+   Warning = 'w',
+   Error   = 'e'
+};
 
 void info(const std::string_view& message, const std::source_location& source = std::source_location::current());
 void warning(const std::string_view& message, const std::source_location& source = std::source_location::current());
@@ -34,5 +43,8 @@ struct Info : public Message{Info(const std::source_location& source_location = 
 struct Warning : public Message{Warning(const std::source_location& source_location = std::source_location::current());};
 struct Error : public Message{Error(const std::source_location& source_location = std::source_location::current());};
 
+using SysClockTimePoint = std::chrono::time_point<std::chrono::system_clock>;
+using ListenerCallback = std::function<void(const SysClockTimePoint&, Level, const std::string&, const std::source_location&)>;
+void registerListenerCallback(const ListenerCallback& cb);
 }
 
