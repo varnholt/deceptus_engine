7845432a927ba2f4213965eaff9af501cd1f82c5
varnholt <matthias.varnholt@gmail.com>
2022-01-17 21:16:42 +0100

    added json support for tweaks, added portal threshold to tweaks

--- src/game/tweaks.h
+++ src/game/tweaks.h
@@ -1,6 +1,8 @@
 #pragma once
 
-class Tweaks
+#include "framework/tools/jsonconfiguration.h"
+
+class Tweaks : public JsonConfiguration
 {
    public:
 
@@ -9,8 +11,13 @@ class Tweaks
       float _bend_down_threshold = 0.6f;
       float _cpan_tolerance_x = 0.2f;
       float _cpan_tolerance_y = 0.2f;
+      float _enter_portal_threshold = -0.6f;
 
    private:
+
       Tweaks() = default;
+      std::string serialize() override;
+      void deserialize(const std::string& data) override;
+      bool initialized = false;
 };
 
