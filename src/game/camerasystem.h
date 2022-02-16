92a41c6e07a3da70f75d9bd73073c116d70e6120
varnholt <matthias.varnholt@gmail.com>
2022-02-16 21:46:06 +0100

    removed unused code

--- src/game/camerasystem.h
+++ src/game/camerasystem.h
@@ -38,8 +38,6 @@ class CameraSystem
       void updateX(const sf::Time& dt);
       void updateY(const sf::Time& dt);
 
-      void updatePlayerFocused();
-
       float _x = 0.0f;
       float _y = 0.0f;
 
@@ -60,8 +58,6 @@ class CameraSystem
 
       bool _no_y_update_triggered = false;
       sf::Time _y_update_start_time;
-
-      bool _player_focused = false;
 
       static CameraSystem sInstance;
 };
