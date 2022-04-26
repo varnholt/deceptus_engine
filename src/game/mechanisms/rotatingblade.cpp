631e4fce89459a2bed6bdf485b560b4d3e934b3b
varnholt <matthias.varnholt@gmail.com>
2022-04-26 22:12:35 +0200

    prepared blade code

--- src/game/mechanisms/rotatingblade.cpp
+++ src/game/mechanisms/rotatingblade.cpp
@@ -1,7 +1,48 @@
 #include "rotatingblade.h"
 
+namespace
+{
+static constexpr auto blade_acceleration = 0.003f;
+static constexpr auto blade_deceleration = 0.003f;
+static constexpr auto blade_rotation_speed = 0.1f;
+}
+
 
 RotatingBlade::RotatingBlade(GameNode* parent)
  : GameNode(parent)
 {
+   setClassName(typeid(RotatingBlade).name());
+}
+
+
+void RotatingBlade::setup(const GameDeserializeData& /*data*/)
+{
+
+}
+
+
+void RotatingBlade::update(const sf::Time& dt)
+{
+  if (_enabled)
+  {
+     _velocity = std::min<float>(1.0f, _velocity + blade_acceleration);
+  }
+  else
+  {
+     _velocity = std::max<float>(0.0f, _velocity - blade_deceleration);
+  }
+
+  _angle += dt.asSeconds() * _velocity * _direction * blade_rotation_speed;
+}
+
+
+void RotatingBlade::draw(sf::RenderTarget& target, sf::RenderTarget& /*normal*/)
+{
+   target.draw(_sprite);
+}
+
+
+void RotatingBlade::setEnabled(bool /*enabled*/)
+{
+
 }
--- src/game/mechanisms/rotatingblade.h
+++ src/game/mechanisms/rotatingblade.h
@@ -3,8 +3,26 @@
 #include "gamemechanism.h"
 #include "gamenode.h"
 
+#include "gamedeserializedata.h"
+
+
 class RotatingBlade : public GameMechanism, public GameNode
 {
 public:
     RotatingBlade(GameNode* parent = nullptr);
+
+    void setup(const GameDeserializeData& data);
+    void update(const sf::Time& dt);
+    void draw(sf::RenderTarget& target, sf::RenderTarget& normal) override;
+    void setEnabled(bool enabled) override;
+
+private:
+    float _angle = 0.0f;
+    float _velocity = 0.0f;
+    float _direction = 1.0f;
+
+    std::shared_ptr<sf::Texture> _texture_map;
+    std::shared_ptr<sf::Texture> _normal_map;
+    sf::Sprite _sprite;
+    sf::IntRect _rectangle;
 };
