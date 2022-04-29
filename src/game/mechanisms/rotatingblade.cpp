8c7252fb92e06cc31e7202a1cddaedeb09f204cb
Matthias Varnholt <matthias.varnholt@gmail.com>
2022-04-29 23:43:37 +0200

    rotating blades are almost done

--- src/game/mechanisms/rotatingblade.cpp
+++ src/game/mechanisms/rotatingblade.cpp
@@ -1,6 +1,8 @@
 #include "rotatingblade.h"
 
+#include "framework/tmxparser/tmxpolygon.h"
 #include "framework/tmxparser/tmxpolyline.h"
+#include "framework/tools/log.h"
 #include "game/texturepool.h"
 
 
@@ -14,6 +16,8 @@ RotatingBlade::RotatingBlade(GameNode* parent)
 {
    setClassName(typeid(RotatingBlade).name());
 
+   setZ(30);
+
    _texture_map = TexturePool::getInstance().get("data/sprites/enemy_rotating_blade.png");
    _sprite.setTexture(*_texture_map.get());
 }
@@ -21,12 +25,19 @@ RotatingBlade::RotatingBlade(GameNode* parent)
 
 void RotatingBlade::setup(const GameDeserializeData& data)
 {
-   if (!data._tmx_object->_polyline)
+   if (!data._tmx_object->_polygon && !data._tmx_object->_polyline)
    {
+      Log::Error() << "the tmx object is neither a polygon or polyline";
       return;
    }
 
-   _path = data._tmx_object->_polyline->_polyline;
+   _path = data._tmx_object->_polygon ? data._tmx_object->_polygon->_polyline : data._tmx_object->_polyline->_polyline;
+   _path_type = data._tmx_object->_polygon ? PathType::Polygon : PathType::Polyline;
+
+   std::transform(_path.begin(), _path.end(), _path.begin(), [data](auto& vec){
+       return vec + sf::Vector2f{data._tmx_object->_x_px, data._tmx_object->_y_px};
+   });
+
    _path_interpolation.addKeys(_path);
 }
 
@@ -42,13 +53,24 @@ void RotatingBlade::update(const sf::Time& dt)
       _velocity = std::max<float>(0.0f, _velocity - _settings._blade_deceleration);
    }
 
+   // exit early if velocity is under a certain threshold
+
+   if (_path_type == PathType::Polyline)
+   {
+      _path_interpolation.updateTime(dt.asSeconds() * _velocity);
+   }
+   else
+   {
+      _path_interpolation.updateTimeLooped(dt.asSeconds() * _velocity);
+   }
+
    _angle += dt.asSeconds() * _velocity * _direction * _settings._blade_rotation_speed;
+   const auto pos = _path_interpolation.computePosition(_path_interpolation.getTime());
 
    _sprite.setRotation(_angle);
-
-   _path_interpolation.updateTime(dt.asSeconds());
-   const auto pos = _path_interpolation.computePosition(_path_interpolation.getTime());
    _sprite.setPosition(pos);
+
+   // _sprite.setPosition(_path.at(0));
 }
 
 
