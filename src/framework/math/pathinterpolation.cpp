24f4ac51628257796860aa8c7ed4e18c1e16c2b1
Matthias Varnholt <matthias.varnholt@gmail.com>
2022-04-29 19:38:04 +0200

    prepared linear interpolation for saw blades

--- src/framework/math/pathinterpolation.cpp
+++ src/framework/math/pathinterpolation.cpp
@@ -1,4 +1,2 @@
 #include "pathinterpolation.h"
 
-
-
--- src/game/mechanisms/rotatingblade.h
+++ src/game/mechanisms/rotatingblade.h
@@ -3,6 +3,7 @@
 #include "gamemechanism.h"
 #include "gamenode.h"
 
+#include "framework/math/pathinterpolation.h"
 #include "gamedeserializedata.h"
 
 #include "SFML/Graphics.hpp"
@@ -23,7 +24,7 @@ public:
    RotatingBlade(GameNode* parent = nullptr);
 
    void setup(const GameDeserializeData& data);
-   void update(const sf::Time& dt);
+   void update(const sf::Time& dt) override;
    void draw(sf::RenderTarget& target, sf::RenderTarget& normal) override;
    void setEnabled(bool enabled) override;
 
@@ -31,11 +32,13 @@ private:
    float _angle = 0.0f;
    float _velocity = 0.0f;
    float _direction = 1.0f;
+   float _time_normalized = 0.0f;
 
    std::shared_ptr<sf::Texture> _texture_map;
    std::shared_ptr<sf::Texture> _normal_map;
    sf::Sprite _sprite;
    sf::IntRect _rectangle;
    std::vector<sf::Vector2f> _path;
+   PathInterpolation<sf::Vector2f> _path_interpolation;
    Settings _settings;
 };
