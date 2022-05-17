9213943385708d6fd096222b02bf7546fdd8ba0d
varnholt <matthias.varnholt@gmail.com>
2022-05-17 20:20:00 +0200

    just preparation for dstars spriteset

--- src/game/mechanisms/collapsingplatform.h
+++ src/game/mechanisms/collapsingplatform.h
@@ -9,10 +9,17 @@ struct TmxObject;
 
 #include "Box2D/Box2D.h"
 #include <filesystem>
+#include <vector>
+
 
 class CollapsingPlatform : public FixtureNode, public GameMechanism
 {
 public:
+
+   struct Block
+   {
+      int32_t _sprite_index = 0;
+   };
 
    CollapsingPlatform(
       GameNode* parent,
@@ -32,15 +39,20 @@ private:
    bool _collapsed = false;
    int32_t _contact_count = 0;
    sf::Time _collapse_time;
+   int32_t _width_tl = 0;
+   float _width_m = 0.0f;
+   float _height_m = 0.0f;
+   std::vector<Block> _blocks;
+   sf::Vector2f _position_px;
 
    // sf
    std::shared_ptr<sf::Texture> _texture;
-   sf::Sprite _sprite;
+   std::vector<sf::Sprite> _sprites;
 
    // b2d
    b2Body* _body = nullptr;
    b2Fixture* _fixture = nullptr;
    b2Vec2 _position_m;
-   b2PolygonShape _shape;
+   b2ChainShape _shape;
 };
 
