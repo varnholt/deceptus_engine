ec83f1195781c65ab1212c723b2623a7c71ecd68
varnholt <matthias.varnholt@gmail.com>
2022-06-07 21:15:04 +0200

    prepared attack animation code

--- src/game/player/playeranimation.h
+++ src/game/player/playeranimation.h
@@ -36,6 +36,8 @@ public:
       bool _wall_sliding = false;
       bool _wall_jump_points_right = false;
       bool _jumping_through_one_way_wall = false;
+      bool _holding_sword = false;
+      bool _attacking = false;
       std::optional<Dash> _dash_dir;
       b2Vec2 _linear_velocity = b2Vec2{0.0f, 0.0f};
       int32_t _jump_frame_count = 0;
@@ -64,69 +66,74 @@ public:
 
 private:
 
-   std::shared_ptr<Animation> _idle_r_2;
-   std::shared_ptr<Animation> _idle_l_2;
-   std::shared_ptr<Animation> _idle_blink_r_2;
-   std::shared_ptr<Animation> _idle_blink_l_2;
+   std::shared_ptr<Animation> _idle_r;
+   std::shared_ptr<Animation> _idle_l;
+   std::shared_ptr<Animation> _idle_blink_r;
+   std::shared_ptr<Animation> _idle_blink_l;
    std::shared_ptr<Animation> _idle_r_tmp;
    std::shared_ptr<Animation> _idle_l_tmp;
 
-   std::shared_ptr<Animation> _bend_down_idle_l_2;
-   std::shared_ptr<Animation> _bend_down_idle_r_2;
-   std::shared_ptr<Animation> _bend_down_idle_blink_l_2;
-   std::shared_ptr<Animation> _bend_down_idle_blink_r_2;
+   std::shared_ptr<Animation> _bend_down_idle_l;
+   std::shared_ptr<Animation> _bend_down_idle_r;
+   std::shared_ptr<Animation> _bend_down_idle_blink_l;
+   std::shared_ptr<Animation> _bend_down_idle_blink_r;
    std::shared_ptr<Animation> _bend_down_idle_l_tmp;
    std::shared_ptr<Animation> _bend_down_idle_r_tmp;
 
-   std::shared_ptr<Animation> _bend_down_r_2;
-   std::shared_ptr<Animation> _bend_down_l_2;
-   std::shared_ptr<Animation> _bend_up_r_2;
-   std::shared_ptr<Animation> _bend_up_l_2;
+   std::shared_ptr<Animation> _bend_down_r;
+   std::shared_ptr<Animation> _bend_down_l;
+   std::shared_ptr<Animation> _bend_up_r;
+   std::shared_ptr<Animation> _bend_up_l;
 
-   std::shared_ptr<Animation> _idle_to_run_r_2;
-   std::shared_ptr<Animation> _idle_to_run_l_2;
-   std::shared_ptr<Animation> _runstop_r_2;
-   std::shared_ptr<Animation> _runstop_l_2;
-   std::shared_ptr<Animation> _run_r_2;
-   std::shared_ptr<Animation> _run_l_2;
+   std::shared_ptr<Animation> _idle_to_run_r;
+   std::shared_ptr<Animation> _idle_to_run_l;
+   std::shared_ptr<Animation> _runstop_r;
+   std::shared_ptr<Animation> _runstop_l;
+   std::shared_ptr<Animation> _run_r;
+   std::shared_ptr<Animation> _run_l;
 
-   std::shared_ptr<Animation> _dash_init_r_2;
-   std::shared_ptr<Animation> _dash_init_l_2;
-   std::shared_ptr<Animation> _dash_r_2;
-   std::shared_ptr<Animation> _dash_l_2;
-   std::shared_ptr<Animation> _dash_stop_r_2;
-   std::shared_ptr<Animation> _dash_stop_l_2;
+   std::shared_ptr<Animation> _dash_init_r;
+   std::shared_ptr<Animation> _dash_init_l;
+   std::shared_ptr<Animation> _dash_r;
+   std::shared_ptr<Animation> _dash_l;
+   std::shared_ptr<Animation> _dash_stop_r;
+   std::shared_ptr<Animation> _dash_stop_l;
 
-   std::shared_ptr<Animation> _crouch_r_2;
-   std::shared_ptr<Animation> _crouch_l_2;
+   std::shared_ptr<Animation> _crouch_r;
+   std::shared_ptr<Animation> _crouch_l;
 
-   std::shared_ptr<Animation> _jump_init_r_2;
-   std::shared_ptr<Animation> _jump_up_r_2;
-   std::shared_ptr<Animation> _jump_midair_r_2;
-   std::shared_ptr<Animation> _jump_down_r_2;
-   std::shared_ptr<Animation> _jump_landing_r_2;
+   std::shared_ptr<Animation> _jump_init_r;
+   std::shared_ptr<Animation> _jump_up_r;
+   std::shared_ptr<Animation> _jump_midair_r;
+   std::shared_ptr<Animation> _jump_down_r;
+   std::shared_ptr<Animation> _jump_landing_r;
 
-   std::shared_ptr<Animation> _jump_init_l_2;
-   std::shared_ptr<Animation> _jump_up_l_2;
-   std::shared_ptr<Animation> _jump_midair_l_2;
-   std::shared_ptr<Animation> _jump_down_l_2;
-   std::shared_ptr<Animation> _jump_landing_l_2;
+   std::shared_ptr<Animation> _jump_init_l;
+   std::shared_ptr<Animation> _jump_up_l;
+   std::shared_ptr<Animation> _jump_midair_l;
+   std::shared_ptr<Animation> _jump_down_l;
+   std::shared_ptr<Animation> _jump_landing_l;
 
-   std::shared_ptr<Animation> _double_jump_r_2;
-   std::shared_ptr<Animation> _double_jump_l_2;
-   std::shared_ptr<Animation> _swim_idle_r_2;
-   std::shared_ptr<Animation> _swim_idle_l_2;
-   std::shared_ptr<Animation> _swim_r_2;
-   std::shared_ptr<Animation> _swim_l_2;
+   std::shared_ptr<Animation> _double_jump_r;
+   std::shared_ptr<Animation> _double_jump_l;
+   std::shared_ptr<Animation> _swim_idle_r;
+   std::shared_ptr<Animation> _swim_idle_l;
+   std::shared_ptr<Animation> _swim_r;
+   std::shared_ptr<Animation> _swim_l;
 
-   std::shared_ptr<Animation> _wallslide_impact_r_2;
-   std::shared_ptr<Animation> _wallslide_impact_l_2;
-   std::shared_ptr<Animation> _wallslide_r_2;
-   std::shared_ptr<Animation> _wallslide_l_2;
-   std::shared_ptr<Animation> _wall_jump_r_2;
-   std::shared_ptr<Animation> _wall_jump_l_2;
-   std::shared_ptr<Animation> _appear_r_2;
-   std::shared_ptr<Animation> _appear_l_2;
+   std::shared_ptr<Animation> _wallslide_impact_r;
+   std::shared_ptr<Animation> _wallslide_impact_l;
+   std::shared_ptr<Animation> _wallslide_r;
+   std::shared_ptr<Animation> _wallslide_l;
+   std::shared_ptr<Animation> _wall_jump_r;
+   std::shared_ptr<Animation> _wall_jump_l;
+   std::shared_ptr<Animation> _appear_r;
+   std::shared_ptr<Animation> _appear_l;
+
+   std::shared_ptr<Animation> _sword_idle_r;
+   std::shared_ptr<Animation> _sword_idle_l;
+   std::shared_ptr<Animation> _sword_attack_r;
+   std::shared_ptr<Animation> _sword_attack_l;
 
    std::shared_ptr<Animation> _death;
 
