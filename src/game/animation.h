531c6d8b4877a870c0ab3bef6bbb08139bef5540
varnholt <matthias.varnholt@gmail.com>
2022-07-08 13:18:18 +0200

    support for multiple/split animations

--- src/game/animation.h
+++ src/game/animation.h
@@ -21,35 +21,45 @@ public:
 
    void draw(sf::RenderTarget& target, sf::RenderStates states = {}) const override;
    void draw(sf::RenderTarget& color, sf::RenderTarget& normal, sf::RenderStates states = {}) const;
+   void drawTree(sf::RenderTarget& target, sf::RenderStates states = {}) const;
+   void drawTree(sf::RenderTarget& color, sf::RenderTarget& normal, sf::RenderStates states = {}) const;
 
    void update(const sf::Time& dt);
-
    void play();
    void pause();
    void stop();
-
    void seekToStart();
+
+   void updateTree(const sf::Time& dt);
+   void playTree();
+   void pauseTree();
+   void stopTree();
+   void seekToStartTree();
+
    void updateVertices(bool resetTime = true);
 
    void setAlpha(uint8_t alpha);
+   void setAlphaTree(uint8_t alpha);
 
    sf::FloatRect getLocalBounds() const;
    sf::FloatRect getGlobalBounds() const;
 
+   void setFrameTimes(const std::vector<sf::Time>& frame_times);
+   size_t getFrameCount() const;
+   void reverse();
+
+   void addChild(const std::shared_ptr<Animation>& child);
+
    std::string _name;
-
    std::vector<sf::IntRect> _frames;
-
    std::shared_ptr<sf::Texture> _color_texture;
    std::shared_ptr<sf::Texture> _normal_texture;
-
    sf::Vertex _vertices[4];
 
    sf::Time _current_time;
    sf::Time _elapsed;
    sf::Time _overall_time;
    HighResDuration _overall_time_chrono;
-
    int32_t _current_frame = 0;
    int32_t _previous_frame = -1;
 
@@ -58,11 +68,7 @@ public:
    bool _reset_to_first_frame = true;
    bool _finished = false;
 
-   void setFrameTimes(const std::vector<sf::Time>& frame_times);
-
-   size_t getFrameCount() const;
-
-   void reverse();
+   std::vector<std::shared_ptr<Animation>> _children;
 
 
 private:
