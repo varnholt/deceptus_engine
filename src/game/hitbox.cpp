11525c8763180a8b66f33559b43d7ff0cc574618
Matthias Varnholt <matthias.varnholt@gmail.com>
2022-08-02 20:06:48 +0200

    trying out sword attack on blob

--- src/game/hitbox.cpp
+++ src/game/hitbox.cpp
@@ -1,11 +1,8 @@
 #include "hitbox.h"
 
-
-Hitbox::Hitbox(const sf::FloatRect& rect)
- : _rect_px(rect)
+Hitbox::Hitbox(const sf::FloatRect& rect, const sf::Vector2f& offset) : _rect_px(rect), _offset_px(offset)
 {
 }
-
 
 sf::FloatRect Hitbox::getRectTranslated() const
 {
