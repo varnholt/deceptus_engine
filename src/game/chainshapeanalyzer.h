5b9ad0c35bd9e8cf5ae0ef7b8d4d8e34f5deba7a
Matthias Varnholt <matthias.varnholt@gmail.com>
2022-08-09 19:48:07 +0200

    added chain shape analyzer to address missing ghost vertices

---
+++ src/game/chainshapeanalyzer.h
@@ -0,0 +1,12 @@
+#ifndef CHAINSHAPEANALYZER_H
+#define CHAINSHAPEANALYZER_H
+
+#include <vector>
+#include "Box2D/Box2D.h"
+
+namespace ChainShapeAnalyzer
+{
+void analyze(std::vector<std::vector<b2Vec2>>& chains);
+};
+
+#endif // CHAINSHAPEANALYZER_H
