3bb4519dbb5471d265b58a0642a013763ee6435c
Matthias Varnholt <matthias.varnholt@gmail.com>
2022-08-12 22:40:33 +0200

    fixed chain shape analyzer

--- src/game/chainshapeanalyzer.cpp
+++ src/game/chainshapeanalyzer.cpp
@@ -2,32 +2,20 @@
 
 #include <algorithm>
 #include <iostream>
-#include <unordered_set>
+#include <set>
 
 namespace
 {
-
-constexpr auto epsilon = 0.001f;
 
 struct IndexedVector
 {
    int32_t _chain_index{0};
    int32_t _vector_index{0};
-   b2Vec2 _pos;
+   std::pair<float, float> _pos;
 
-   bool operator==(const IndexedVector& v) const
+   bool operator<(const IndexedVector& v) const
    {
-      return (fabs(_pos.x - v._pos.x) < epsilon && fabs(_pos.y - v._pos.y) < epsilon);
-   }
-};
-
-struct HashFunction
-{
-   size_t operator()(const IndexedVector& v) const
-   {
-      const auto hash_x = std::hash<float>()(v._pos.x);
-      const auto hash_y = std::hash<float>()(v._pos.y) << 1;
-      return hash_x ^ hash_y;
+      return _pos < v._pos;
    }
 };
 
@@ -35,7 +23,8 @@ struct HashFunction
 
 void ChainShapeAnalyzer::analyze(std::vector<std::vector<b2Vec2>>& chains)
 {
-   static std::unordered_set<IndexedVector, HashFunction> _indexed_vectors;
+   static std::set<IndexedVector> _indexed_vectors;
+   _indexed_vectors.clear();
 
    int32_t chain_index = 0;
    for (const auto& chain : chains)
@@ -46,18 +35,18 @@ void ChainShapeAnalyzer::analyze(std::vector<std::vector<b2Vec2>>& chains)
          chain.end(),
          [chain_index, &vector_index](const auto& v)
          {
-            const auto iv = IndexedVector{chain_index, vector_index, v};
+            const auto iv = IndexedVector{chain_index, vector_index, {v.x, v.y}};
             const auto it = _indexed_vectors.find(iv);
 
             if (it == _indexed_vectors.end())
             {
                _indexed_vectors.insert(iv);
             }
-            else
+            else if (chain_index != it->_chain_index)
             {
-               std::cout << "chain " << chain_index << ", vector: " << vector_index << ", pos(" << iv._pos.x << ", " << iv._pos.y
-                         << ") collides with chain: " << it->_chain_index << ", vector: " << it->_vector_index << ", pos(" << it->_pos.x
-                         << ", " << it->_pos.y << ")" << std::endl;
+               std::cout << "chain " << chain_index << ", vector: " << vector_index << ", pos(" << iv._pos.first << ", " << iv._pos.second
+                         << ") collides with chain: " << it->_chain_index << ", vector: " << it->_vector_index << ", pos(" << it->_pos.first
+                         << ", " << it->_pos.second << ")" << std::endl;
             }
             vector_index++;
          }
