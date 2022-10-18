02870cfe578a54e01fc44497488260f4e05aa820
Matthias Varnholt <matthias@cercare-medical.com>
2022-10-18 22:40:28 +0200

    started bubbles

---
+++ src/game/waterbubbles.cpp
@@ -0,0 +1,15 @@
+#include "waterbubbles.h"
+
+#include "atmosphere.h"
+
+WaterBubbles::WaterBubbles()
+{
+}
+
+void WaterBubbles::draw(sf::RenderTarget& target, sf::RenderTarget& normal)
+{
+}
+
+void WaterBubbles::update(const sf::Time& dt)
+{
+}
