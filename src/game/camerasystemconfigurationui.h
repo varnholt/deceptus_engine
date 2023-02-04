67340748ca1f7d16306e09ef55a424a6492ba9a7
Matthias Varnholt <matthias@cercare-medical.com>
2023-02-04 12:47:56 +0100

    add ui for camera system

---
+++ src/game/camerasystemconfigurationui.h
@@ -0,0 +1,20 @@
+#ifndef CAMERASYSTEMCONFIGURATIONUI_H
+#define CAMERASYSTEMCONFIGURATIONUI_H
+
+#include <memory>
+#include "SFML/Graphics.hpp"
+
+class CameraSystemConfigurationUi
+{
+public:
+   CameraSystemConfigurationUi();
+
+   void processEvents();
+   void draw();
+   void close();
+
+   std::unique_ptr<sf::RenderWindow> _render_window;
+   sf::Clock _clock;
+};
+
+#endif // CAMERASYSTEMCONFIGURATIONUI_H
