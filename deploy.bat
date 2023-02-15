6a3a91029e109f5d1d7b977864c6b24ced776700
Matthias Varnholt <matthias.varnholt@gmail.com>
2023-02-15 22:30:28 +0100

    move level selector

--- deploy.bat
+++ deploy.bat
@@ -7,7 +7,7 @@ xcopy /Y sfml\bin\*.dll ..\build
 xcopy /Y SDL\lib\x64\*.dll ..\build
 xcopy /E /Y data ..\build\data
 xcopy /E /Y tools ..\build\tools
-xcopy /E /Y tools\level_selector\release\level_selector.exe ..\build
+xcopy /E /Y tools\level_selector\level_selector.exe ..\build
 xcopy /Y deceptus.exe ..\build
 
 IF EXIST deceptus.pdb xcopy /C /Y deceptus.pdb ..\build
