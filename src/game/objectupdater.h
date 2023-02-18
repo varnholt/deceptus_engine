8a81cf20cb380222ef9b5a90a5da0ab89a165d15
Matthias Varnholt <matthias@cercare-medical.com>
2023-02-18 22:15:21 +0100

    removed thread from objectupdater

--- src/game/objectupdater.h
+++ src/game/objectupdater.h
@@ -11,25 +11,18 @@
 class ObjectUpdater
 {
 public:
-   ObjectUpdater();
-   ~ObjectUpdater();
+   ObjectUpdater() = default;
+   virtual ~ObjectUpdater();
 
-   void run();
-   void stop();
-   bool stopped() const;
+   void update();
 
    void setPlayerPosition(const sf::Vector2f& position);
-
-   void setEnemies(const std::vector<std::shared_ptr<LuaNode>>& enemies);
    void setMechanisms(const std::vector<std::vector<std::shared_ptr<GameMechanism>>*>& mechanisms);
 
 private:
    void updateVolume(const std::shared_ptr<GameMechanism>& mechanism);
    float computeDistanceToPlayerPx(const std::shared_ptr<GameMechanism>& mechanism);
 
-   mutable std::mutex _mutex;
-
-   std::vector<std::shared_ptr<LuaNode>> _enemies;
    std::vector<std::vector<std::shared_ptr<GameMechanism>>*> _mechanisms;
    sf::Vector2f _player_position;
 
