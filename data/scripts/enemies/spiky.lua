137937ea189c64b9bcbafecfab7d1671f6629cb8
Matthias Varnholt <matthias.varnholt@gmail.com>
2023-03-09 20:57:45 +0100

    implemented all spiky cycles

--- data/scripts/enemies/spiky.lua
+++ data/scripts/enemies/spiky.lua
@@ -4,13 +4,13 @@
 -- row 3: walk right
 -- row 4: hide left
 -- row 5: hide right
--- row 6, 7: idle left
--- row 8, 9: idle right
+-- row 6, 7: hide idle left
+-- row 8, 9: hide idle right
 --
--- hide left: 48-59
--- hide right: 60-71
--- reveal left: 51-48
--- reveal right: 63-60
+-- hide left: 48-59 (row 4)
+-- hide right: 60-71 (row 5)
+-- reveal left: 51-48 (row 4, first 4 sprites, inverse)
+-- reveal right: 63-60 (row 5, first 4 sprites, inverse)
 
 -- 12 sprites per row
 
@@ -41,8 +41,12 @@ CYCLE_HIDE_IDLE = 3
 CYCLE_APPEAR = 4
 
 CYCLE_LENGTHS =  {12, 12, 12, 15, 4}
+CYCLE_ROWS_LEFT = {0, 2, 4, 6, 4}
+CYCLE_ROWS_RIGHT = {1, 3, 5, 8, 5}
 ANIMATION_SPEEDS = {10.0, 20.0, 10.0, 10.0, 10.0}
 SPRITE_SIZE = 48
+SPRITES_PER_ROW = 12
+
 
 ------------------------------------------------------------------------------------------------------------------------
 _keys_pressed = 0
@@ -61,6 +65,7 @@ _cycle_change_requested = false
 _requested_cycle = 0
 _elapsed = 0.0
 _idle = false
+_counter_to_reveal = 0
 
 
 ------------------------------------------------------------------------------------------------------------------------
@@ -143,14 +148,18 @@ function updateSprite(dt)
       _current_sprite = sprite_index
 
       -- appear is an inverse of hide
-      if (next_animation_flag_inverse) then
+      if (_animation_flag_inverse) then
          sprite_index = getMaxCycle() - sprite_index - 1
       end
+
+      -- need to wrap sprites as sprite sets are longer than a single row :/
+      sprite_index = sprite_index % SPRITES_PER_ROW
+      wrapped_y = sprite_index // SPRITES_PER_ROW
 
       updateSpriteRect(
          0,
          sprite_index * SPRITE_SIZE,
-         getSpriteOffsetY(),
+         getSpriteOffsetY() + wrapped_y * SPRITE_SIZE,
          SPRITE_SIZE,
          SPRITE_SIZE
       )
@@ -160,7 +169,7 @@ end
 
 ------------------------------------------------------------------------------------------------------------------------
 function getSpriteOffsetY()
-   return (_current_cycle * 2 + (_points_to_left and 0 or 1)) * SPRITE_SIZE
+   return _points_to_left and (CYCLE_ROWS_LEFT[_current_cycle + 1] * SPRITE_SIZE) or (CYCLE_ROWS_RIGHT[_current_cycle + 1] * SPRITE_SIZE)
 end
 
 
@@ -266,15 +275,27 @@ function decide(dt)
       next_animation_flag_inverse = false
 
       if (_cycle_change_requested) then
+         print("cycle change requested")
          next_cycle = _requested_cycle
+         _cycle_change_requested = false
       end
 
       if (_current_cycle == CYCLE_HIDE) then
+         print("go to hide idle")
          next_cycle = CYCLE_HIDE_IDLE
       end
 
       if (_current_cycle == CYCLE_HIDE_IDLE) then
          -- wait a bit
+         _counter_to_reveal = _counter_to_reveal - 1
+         if (_counter_to_reveal == 0) then
+            next_cycle = CYCLE_APPEAR
+            next_animation_flag_inverse = true
+         end
+      end
+
+      if (_current_cycle == CYCLE_APPEAR) then
+         next_cycle = CYCLE_WALK
       end
 
       if (next_cycle ~= _current_cycle) then
@@ -290,4 +311,6 @@ end
 function hit(damage_value)
    _cycle_change_requested = true
    _requested_cycle = CYCLE_HIDE
+   _counter_to_reveal = math.random(20,30)
+   print("hide")
 end
