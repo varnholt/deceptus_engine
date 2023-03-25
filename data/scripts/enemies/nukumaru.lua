b0a877cb13d30ba775a69920783fdd4940d36cdf
Matthias Varnholt <matthias.varnholt@gmail.com>
2023-03-25 19:55:02 +0100

    nukumaru is done

--- data/scripts/enemies/nukumaru.lua
+++ data/scripts/enemies/nukumaru.lua
@@ -1,79 +1,68 @@
 require "data/scripts/enemies/constants"
-require "data/scripts/enemies/interpolation"
-v2d = require "data/scripts/enemies/vectorial2"
-
 
 ------------------------------------------------------------------------------------------------------------------------
 properties = {
    sprite = "data/sprites/enemy_nukumaru.png",
    damage = 40,
-   restitution = 1.0,
+   restitution = 1.0, -- maximum bounce
    density = 0.01
 }
 
-
 SPRITE_WIDTH = 128
 SPRITE_HEIGHT = 128
+SPRITE_COUNT = 60
+ANIMATION_SPEED = 100.0
 
-_position = v2d.Vector2D(0, 0)
-_player_position = v2d.Vector2D(0, 0)
 _elapsed = 0.0
-_initialized_impulse = false
-_impulses = 0
-
+_tick_count = 0
+_sprite_index = -1
+_impulse_x = 0.03
+_impulse_y = 0.03
 
 ------------------------------------------------------------------------------------------------------------------------
 function initialize()
 
-   addShapeCircle(0.8, 0.0, -0.06)                        -- radius, x, y
+   addShapeCircle(0.6, 0.0, -0.06)                        -- radius, x, y
    updateSpriteRect(0, 0, 0, SPRITE_WIDTH, SPRITE_HEIGHT) -- id, x, y, width, height
-
-   -- this will need a few hitboxes to be more or less accurate
-   --
    addHitbox(-18, -18, 36, 36)                            -- x offset, y offset, width, height
-
-   setGravityScale(0.0)
-
+   setZ(30)                                               -- place in foreground
+   setGravityScale(0.0)                                   -- nukumaru is not impacted by gravity
 end
-
-
-------------------------------------------------------------------------------------------------------------------------
-function retrieveProperties()
-   updateProperties(properties)
-end
-
-
-------------------------------------------------------------------------------------------------------------------------
-function writeProperty(key, value)
-end
-
-
-------------------------------------------------------------------------------------------------------------------------
-function movedTo(x, y)
-   _position = v2d.Vector2D(x, y)
-end
-
-
-------------------------------------------------------------------------------------------------------------------------
-function playerMovedTo(x, y)
-   _player_position = v2d.Vector2D(x, y)
-end
-
 
 ------------------------------------------------------------------------------------------------------------------------
 function update(dt)
    _elapsed = _elapsed + dt
 
-   if (_impulses < 20) then
-      applyForce(2.0, 0.0)
-      -- _initialized_impulse = true
-      _impulses = _impulses + 1
+   -- wait just a tiny bit before applying that impulse
+   _tick_count = _tick_count + 1
+   if (_tick_count == 10) then
+      applyLinearImpulse(_impulse_x, _impulse_y)
+   end
+
+   -- update sprite
+   sprite_index = math.floor(math.fmod(_elapsed * ANIMATION_SPEED, SPRITE_COUNT))
+   if (sprite_index ~= _sprite_index) then
+      _sprite_index = sprite_index
+      updateSpriteRect(
+         0,
+         _sprite_index * SPRITE_WIDTH,
+         0,
+         SPRITE_WIDTH,
+         SPRITE_HEIGHT
+      ) -- id, x, y, width, height
    end
 end
 
-
 ------------------------------------------------------------------------------------------------------------------------
-function setStartPosition(x, y)
-   _jump_start_position = v2d.Vector2D(x, y)
+function writeProperty(key, value)
+   if (key == "impulse_x") then
+      _impulse_x = value
+   elseif (key == "impulse_y") then
+      _impulse_y = value
+   end
 end
 
+------------------------------------------------------------------------------------------------------------------------
+function retrieveProperties()
+   updateProperties(properties)
+end
