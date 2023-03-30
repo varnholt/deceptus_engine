8efed609ad63cf6076d73d1e838a64fd2c44fc0e
Matthias Varnholt <matthias@cercare-medical.com>
2023-03-30 22:04:36 +0200

    prototype is done

--- lab/watersurface/widget.h
+++ lab/watersurface/widget.h
@@ -1,7 +1,10 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 
+#include <QElapsedTimer>
 #include <QWidget>
+
+#include "watersurface.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
@@ -17,8 +20,11 @@ public:
 
 protected:
    void paintEvent(QPaintEvent* event) override;
+   void mousePressEvent(QMouseEvent* event) override;
 
 private:
    Ui::Widget *ui;
+   WaterSurface _surface;
+   QElapsedTimer _elapsed;
 };
 #endif // WIDGET_H
