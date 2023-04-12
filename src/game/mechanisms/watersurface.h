4516b95a45841bd83ff143e12db5296a17f46308
Matthias Varnholt <matthias.varnholt@gmail.com>
2023-04-12 23:59:13 +0200

    added support for splash emitters

--- src/game/mechanisms/watersurface.h
+++ src/game/mechanisms/watersurface.h
@@ -37,13 +37,20 @@ public:
 
    struct SplashEmitter
    {
-      float _x_from{0.0f};
-      float _x_to{0.0f};
+      float _x_from_px{0.0f};
+      float _x_to_px{0.0f};
       float _interval_min_s{0.0f};
       float _interval_max_s{0.0f};
-      float _strength{0.0f};
-      float _interval_s{0.0f};
+      float _velocity{0.0f};
+      float _width_px{0.0f};
+      int32_t _count{1};
+
       float _elapsed_s{0.0f};
+      float _interval_s{0.0f};
+
+      std::string _parent_reference;
+      sf::FloatRect _bounding_box;
+      float _x_offset_to_parent_px{0.0f};
    };
 
    WaterSurface(GameNode* parent, const GameDeserializeData& data);
@@ -53,6 +60,8 @@ public:
    std::optional<sf::FloatRect> getBoundingBoxPx() override;
 
    void splash(int32_t index, float velocity);
+   static void addEmitter(GameNode* parent, const GameDeserializeData& data);
+   static void merge();
 
 private:
    void updateVertices(int32_t start_index = 0);
