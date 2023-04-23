fa993abe4151bcf737f5e85c088fceb4da6cd2e4
Matthias Varnholt <matthias.varnholt@gmail.com>
2023-04-23 12:48:36 +0200

    refactoring: skills and health are no longer extras

--- src/game/skill.cpp
+++ src/game/skill.cpp
@@ -1,17 +1,15 @@
-#include "extraskill.h"
+#include "skill.h"
 
 using json = nlohmann::json;
 
-
-void to_json(nlohmann::json& j, const ExtraSkill& d)
+void to_json(nlohmann::json& j, const Skill& d)
 {
    j = json{
       {"skills", d._skills},
    };
 }
 
-
-void from_json(const nlohmann::json& j, ExtraSkill& d)
+void from_json(const nlohmann::json& j, Skill& d)
 {
    d._skills = j.at("skills").get<int32_t>();
 }
