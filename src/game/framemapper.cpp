41781eccca6f038851171d9cd624ef5643ae75cc
Matthias Varnholt <matthias.varnholt@gmail.com>
2023-05-07 13:28:18 +0200

    prepared class to support varying frame durations

--- src/game/framemapper.cpp
+++ src/game/framemapper.cpp
@@ -1,14 +1,2 @@
 
 #include "framemapper.h"
-
-FrameMapper::FrameMapper()
-{
-}
-
-void to_json(nlohmann::json& j, const FrameMapper& d)
-{
-}
-
-void from_json(const nlohmann::json& j, FrameMapper& d)
-{
-}
