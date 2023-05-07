0917bfdad92da82b49a5d6c6a757b279ba47aff4
Matthias Varnholt <matthias@cercare-medical.com>
2023-05-07 13:28:18 +0200

    prepared class to support varying frame durations

--- src/game/framemapper.h
+++ src/game/framemapper.h
@@ -1,23 +1,45 @@
-
 #ifndef FRAMEMAPPER_H
 #define FRAMEMAPPER_H
 
 #include <chrono>
+#include <iostream>
 #include <vector>
 
 #include "json/json.hpp"
 
+template <class T>
 class FrameMapper
 {
 public:
-   FrameMapper();
+   FrameMapper(std::initializer_list<T> values, int32_t offset = 0) : _frame_durations(values), _offset(offset)
+   {
+      T sum{};
+      for (const auto val : _frame_durations)
+      {
+         _frame_durations_accumulated.push_back(sum);
+         sum += val;
+      }
+   }
 
-   using HighResDuration = std::chrono::high_resolution_clock::duration;
+   int32_t operator[](const T& frame)
+   {
+      const auto it = std::adjacent_find(
+         _frame_durations_accumulated.begin(),
+         _frame_durations_accumulated.end(),
+         [&frame](const auto& /*prev*/, const auto& current) { return (frame < current); }
+      );
 
-   std::vector<HighResDuration> _frame_durations;
+      if (it != _frame_durations_accumulated.end())
+      {
+         return static_cast<int32_t>(_offset + std::distance(_frame_durations_accumulated.begin(), it));
+      }
+
+      return static_cast<int32_t>(_offset + _frame_durations_accumulated.size() - 1);
+   }
+
+   std::vector<T> _frame_durations;
+   std::vector<T> _frame_durations_accumulated;
+   int32_t _offset{};
 };
-
-void to_json(nlohmann::json& j, const FrameMapper& d);
-void from_json(const nlohmann::json& j, FrameMapper& d);
 
 #endif // FRAMEMAPPER_H
--- src/game/test.cpp
+++ src/game/test.cpp
@@ -5,6 +5,7 @@
 #include "framework/math/fbm.h"
 #include "framework/math/maptools.h"
 #include "framework/math/pathinterpolation.h"
+#include "game/framemapper.h"
 
 #include <iostream>
 
@@ -15,30 +16,66 @@ void testFbm()
 
 void testBresenham()
 {
-   auto f = [](int32_t x, int32_t y) -> bool {
+   auto f = [](int32_t x, int32_t y) -> bool
+   {
       std::cout << x << ", " << y << std::endl;
       return false;
    };
 
-   MapTools::lineCollide(0,0, 10,5, f);
+   MapTools::lineCollide(0, 0, 10, 5, f);
 }
 
+void testFrameMapper()
+{
+   FrameMapper<int32_t> frame_times{{50, 50, 50, 120, 3000}, 0};
+
+   auto index = 0;
+   for (auto val : frame_times._frame_durations_accumulated)
+   {
+      std::cout << index << ": " << val << std::endl;
+      index++;
+   }
+
+   // 0: 50 -> 0..50
+   // 1: 50 -> 51..100
+   // 2: 50 -> 101..151
+   // 3: 120 -> 151..270
+   // 4: 3000 -> 270..3000
+
+   // 0: 50
+   // 1: 100
+   // 2: 150
+   // 3: 270
+   // 4: 3270
+
+   // 0 at 0
+   // 20 at 0
+   // 100 at 1
+   // 500 at 3
+   // 10000 at 4
+
+   std::vector<int32_t> search_values{0, 20, 100, 500, 10000};
+   for (auto search_value : search_values)
+   {
+      std::cout << search_value << " at " << frame_times[search_value] << std::endl;
+   }
+}
 
 void Test::testInterpolation()
 {
-    PathInterpolation<sf::Vector2f> interpolation;
-    std::vector<sf::Vector2f> positions;
-    positions.push_back({0.0f, 0.0f});
-    positions.push_back({5.0f, 5.0f});
-    positions.push_back({0.0f, 0.0f});
-    interpolation.addKeys(positions, 5, Easings::Type::EaseInCubic);
+   PathInterpolation<sf::Vector2f> interpolation;
+   std::vector<sf::Vector2f> positions;
+   positions.push_back({0.0f, 0.0f});
+   positions.push_back({5.0f, 5.0f});
+   positions.push_back({0.0f, 0.0f});
+   interpolation.addKeys(positions, 5, Easings::Type::EaseInCubic);
 
-    for (const auto& k : interpolation.getTrack())
-    {
-        std::cout << k._time_value << ": " << k._pos.x << ", " << k._pos.y << std::endl;
-    }
+   for (const auto& k : interpolation.getTrack())
+   {
+      std::cout << k._time_value << ": " << k._pos.x << ", " << k._pos.y << std::endl;
+   }
 
-    std::cout << Easings::getNameFromEnum<float>(Easings::Type::EaseInCirc) << std::endl;
+   std::cout << Easings::getNameFromEnum<float>(Easings::Type::EaseInCirc) << std::endl;
 }
 
 Test::Test()
@@ -46,5 +83,6 @@ Test::Test()
    // testBresenham();
    // testInterpolation();
    // testFbm();
+   // testFrameMapper();
    // exit(0);
 }
