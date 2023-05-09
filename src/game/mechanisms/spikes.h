3002c9ae4a01287e63f60a05d388260d36ebcbf2
Matthias Varnholt <matthias.varnholt@gmail.com>
2023-05-09 13:49:33 +0200

    remove dynamic frame speeds for now

--- src/game/mechanisms/spikes.h
+++ src/game/mechanisms/spikes.h
@@ -87,7 +87,4 @@ private:
    Orientation _orientation = Orientation::Invalid;
    Config _config;
    int32_t _instance_id{0};
-
-   FrameMapper<int32_t> _frame_times_open_ms{{50, 50, 50, 120, 3000}, 1};
-   FrameMapper<int32_t> _frame_times_close_ms{{80, 30, 30, 30, 30, 30, 30, 30, 3000}, 6};  // first frame (60) skipped
 };
