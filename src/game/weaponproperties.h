61db64728e0ad0395036a75a867efa141b6991ef
Matthias Varnholt <matthias.varnholt@gmail.com>
2023-05-23 22:41:27 +0200

    make weapon properties templated

--- src/game/weaponproperties.h
+++ src/game/weaponproperties.h
@@ -12,22 +12,21 @@ class b2Shape;
 struct WeaponProperties
 {
    using PropertyTypes = std::variant<std::string, int32_t, float, bool>;
-
-   b2Body* _parent_body{nullptr};
-   mutable std::unique_ptr<b2Shape> _shape;
-
-   PropertyTypes read(const std::string& key, const PropertyTypes& default_value) const
+   template <class T>
+   T read(const std::string& key, T default_value) const
    {
       const auto it = _properties.find(key);
       if (it != _properties.end())
       {
-         return it->second;
+         return std::get<T>(it->second);
       }
 
       return default_value;
    }
 
    std::unordered_map<std::string, PropertyTypes> _properties;
+   b2Body* _parent_body{nullptr};
+   mutable std::unique_ptr<b2Shape> _shape;
 };
 
 #endif // WEAPONPROPERTIES_H
