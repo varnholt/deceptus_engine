9fd9f06709c5465de96b0abf80ee6441b220fa6c
Matthias Varnholt <matthias@cercare-medical.com>
2023-05-29 21:20:58 +0200

    prepare stamina

--- src/game/health.h
+++ src/game/health.h
@@ -2,6 +2,8 @@
 
 #include <cstdint>
 
+#include <SFML/Graphics.hpp>
+
 #include "json/json.hpp"
 
 struct Health
@@ -11,11 +13,14 @@ struct Health
    void reset();
 
    void addHealth(int32_t health);
+   void update(const sf::Time& dt);
 
    int32_t _life_count = 5;
 
    int32_t _health = 12;
    int32_t _health_max = 12;
+
+   float _stamina{1.0f};
 };
 
 void to_json(nlohmann::json& j, const Health& d);
