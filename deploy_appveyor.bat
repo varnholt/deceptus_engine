1dbc1cc486c0f1bc72108a3f58000b21eae6d6a4
Matthias Varnholt <matthias@cercare-medical.com>
2023-07-01 18:16:29 +0200

    upgrade to sfml 2.6

--- deploy_appveyor.bat
+++ deploy_appveyor.bat
@@ -2,11 +2,12 @@ IF NOT EXIST build mkdir build
 IF NOT EXIST build\data mkdir build\data
 IF NOT EXIST build\tools mkdir build\tools
 
-xcopy /Y lib64\*.dll build
-xcopy /Y sfml\bin\*.dll build
-xcopy /Y SDL\lib\x64\*.dll build
+xcopy /Y thirdparty\lua\lib64\*.dll build
+xcopy /Y thirdparty\sfml\bin\*.dll build
+xcopy /Y thirdparty\sdl\lib\x64\*.dll build
 xcopy /E /Y data build\data
 xcopy /E /Y tools build\tools
+xcopy /E /Y tools\level_selector\level_selector.exe build
 xcopy /Y deceptus.exe build
 
 IF EXIST deceptus.pdb xcopy /C /Y deceptus.pdb build
