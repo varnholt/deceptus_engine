a6e658fa56d18901dcab6c22ef52dee95c8ba2a8
Matthias Varnholt <matthias.varnholt@gmail.com>
2023-08-12 22:37:41 +0200

    enable all warnings

--- src/game/player/playerclimb.cpp
+++ src/game/player/playerclimb.cpp
@@ -29,213 +29,213 @@ void PlayerClimb::update(b2Body* player_body, bool in_air)
 {
    // disable wallclimb entirely for now - nobody seems to want it.
    return;
-   
-   if (!(SaveState::getPlayerInfo()._extra_table._skills._skills & static_cast<int32_t>(Skill::SkillType::WallClimb)))
-   {
-      return;
-   }
 
-   _keys_pressed = _controls->getKeysPressed();
-
-   // http://www.iforce2d.net/b2dtut/world-querying
-
-   //   // player must indicate the direction he wants to go to
-   //   bool rightPressed = mKeysPressed & KeyPressedRight;
-   //   bool leftPressed = mKeysPressed & KeyPressedLeft;
-   //   if (!(leftPressed || rightPressed))
-   //   {
-   //      removeJoint();
-   //      return;
-   //   }
-
-   // if player is standing somewhere, remove joint
-   if (!in_air)
-   {
-      removeClimbJoint();
-      return;
-   }
-
-   // remove that joint if player is moving 'up'
-   if (player_body->GetLinearVelocity().y < -0.51f)
-   {
-      removeClimbJoint();
-      return;
-   }
-
-   // hold
-   if (_climb_joint)
-   {
-      // remove that joint if it points down from the player perspective
-      auto jointDir = (_climb_joint->GetAnchorA() - _climb_joint->GetAnchorB());
-      if (jointDir.y < -0.1f)
-      {
-         removeClimbJoint();
-      }
-      // printf("joint dir: %f \n", jointDir.y);
-
-      return;
-   }
-
-   // there's nothing to do if there is a climb joint already
-   if (_climb_joint)
-   {
-      return;
-   }
-
-   // TODO: generate accurate aabb to simulate player 'hands'
-   //   float smallerX = 0.0f;
-   //   float smallerY = 0.0f;
-   //   float largerX = 0.0f;
-   //   float largerY = 0.0f;
+   // if (!(SaveState::getPlayerInfo()._extra_table._skills._skills & static_cast<int32_t>(Skill::SkillType::WallClimb)))
+   // {
+   //    return;
+   // }
    //
-   //   b2Vec2 lower(smallerX, smallerY);
-   //   b2Vec2 upper(largerX, largerY);
+   // _keys_pressed = _controls->getKeysPressed();
    //
-   //   b2AABB aabb;
-   //   aabb.lowerBound = lower;
-   //   aabb.upperBound = upper;
-
-   // query nearby region
-   PlayerAABBQueryCallback queryCallback;
-
-   // player aabb is: 19.504843, 158.254532 to 19.824877 158.966980
-   // x: 0.320034 * 0.5 -> 0.1600171
-   // y: 0.712448 * 0.5 -> 0.356224
-   b2AABB aabb;
-   float w = 0.1600171f;
-   float h = 0.356224f;
-   b2Vec2 center = player_body->GetWorldCenter();
-   aabb.lowerBound = b2Vec2(center.x - w, center.y - h);
-   aabb.upperBound = b2Vec2(center.x + w, center.y + h);
-
-   //   b2AABB aabb;
-   //   aabb.lowerBound = b2Vec2(FLT_MAX,FLT_MAX);
-   //   aabb.upperBound = b2Vec2(-FLT_MAX,-FLT_MAX);
-   //   auto fixture = mBody->GetFixtureList();
-   //   while (fixture)
-   //   {
-   //       aabb.Combine(aabb, fixture->GetAABB(0));
-   //       fixture = fixture->GetNext();
-   //   }
-
-   // printf("player aabb is: %f, %f to %f %f\n", aabb.lowerBound.x, aabb.lowerBound.y, aabb.upperBound.x, aabb.upperBound.y);
-
-   player_body->GetWorld()->QueryAABB(&queryCallback, aabb);
-
-   // std::remove_if(queryCallback.foundBodies.begin(), queryCallback.foundBodies.end(), [this](b2Body* body){return body == mBody;});
-   queryCallback._bodies.erase(player_body);
-
-   // printf("bodies in range:\n");
-   for (auto body : queryCallback._bodies)
-   {
-      if (body->GetType() == b2_staticBody)
-      {
-         // printf("- static body: %p\n", body);
-
-         auto found = false;
-         auto dist_max = 0.16f;
-         auto fixture = body->GetFixtureList();
-
-         while (fixture)
-         {
-            auto shape = dynamic_cast<b2ChainShape*>(fixture->GetShape());
-
-            if (shape)
-            {
-               b2Vec2 closest;
-
-               auto edge_length_minimum = 1000.0f;
-               for (auto index = 0; index < shape->m_count; index++)
-               {
-                  auto curr = shape->m_vertices[index];
-                  auto edge_length_squared = (aabb.GetCenter() - curr).LengthSquared();
-
-                  if (edge_length_squared >= dist_max)
-                  {
-                     continue;
-                  }
-
-                  // does player point to edge direction?
-                  if (!edgeMatchesMovement(aabb.GetCenter() - curr))
-                  {
-                     continue;
-                  }
-
-                  // joint in spe needs to point up, since we're holding somewhere4
-                  auto joint_dir = (aabb.GetCenter() - curr);
-                  if (joint_dir.y <= 0.0f)
-                  {
-                     continue;
-                  }
-
-                  // is the analyzed edge actually climbable?
-                  if (!isClimbableEdge(shape, index))
-                  {
-                     continue;
-                  }
-
-                  // printf("joint dir: %f \n", jointDir.y);
-
-                  // if all that is the case, we have a joint
-                  if (edge_length_squared < edge_length_minimum)
-                  {
-                     edge_length_minimum = edge_length_squared;
-                     closest = curr;
-                     found = true;
-                  }
-               }
-
-               // printf("    - closest vtx: %f, %f, distance: %f\n", closest.x, closest.y, distMin);
-
-               // situation a)
-               //
-               //    v
-               //    +------+ prev/next, same y, greater x
-               //    |
-               //    |
-               //    +
-               //  prev/next
-               //
-               //  same x, greater y
-               //
-               //
-               // situation b)
-               //                                        v
-               //    prev/next, same y, smaller x +------+
-               //                                        |
-               //                                        |
-               //                                        +
-               //                                      prev/next
-               //
-               //                                      same x, greater y
-               //
-               //
-               // prev or current needs to have greater y
-               // other vertex must have different x
-
-               if (found)
-               {
-                  b2DistanceJointDef joint_def;
-                  joint_def.Initialize(player_body, body, aabb.GetCenter(), closest);
-                  joint_def.collideConnected = true;
-                  // jointDefinition.dampingRatio = 0.5f;
-                  // jointDefinition.frequencyHz = 5.0f;
-                  // jointDefinition.length = 0.01f;
-
-                  Audio::getInstance().playSample({"impact.wav"});
-                  _climb_joint = player_body->GetWorld()->CreateJoint(&joint_def);
-               }
-
-               // no need to continue processing
-               break;
-            }
-
-            fixture = fixture->GetNext();
-         }
-      }
-   }
-
-   // mDistanceJoint->SetLength(distanceJoint.GetLength() * 0.99f);
+   // // http://www.iforce2d.net/b2dtut/world-querying
+   //
+   // //   // player must indicate the direction he wants to go to
+   // //   bool rightPressed = mKeysPressed & KeyPressedRight;
+   // //   bool leftPressed = mKeysPressed & KeyPressedLeft;
+   // //   if (!(leftPressed || rightPressed))
+   // //   {
+   // //      removeJoint();
+   // //      return;
+   // //   }
+   //
+   // // if player is standing somewhere, remove joint
+   // if (!in_air)
+   // {
+   //    removeClimbJoint();
+   //    return;
+   // }
+   //
+   // // remove that joint if player is moving 'up'
+   // if (player_body->GetLinearVelocity().y < -0.51f)
+   // {
+   //    removeClimbJoint();
+   //    return;
+   // }
+   //
+   // // hold
+   // if (_climb_joint)
+   // {
+   //    // remove that joint if it points down from the player perspective
+   //    auto jointDir = (_climb_joint->GetAnchorA() - _climb_joint->GetAnchorB());
+   //    if (jointDir.y < -0.1f)
+   //    {
+   //       removeClimbJoint();
+   //    }
+   //    // printf("joint dir: %f \n", jointDir.y);
+   //
+   //    return;
+   // }
+   //
+   // // there's nothing to do if there is a climb joint already
+   // if (_climb_joint)
+   // {
+   //    return;
+   // }
+   //
+   // // TODO: generate accurate aabb to simulate player 'hands'
+   // //   float smallerX = 0.0f;
+   // //   float smallerY = 0.0f;
+   // //   float largerX = 0.0f;
+   // //   float largerY = 0.0f;
+   // //
+   // //   b2Vec2 lower(smallerX, smallerY);
+   // //   b2Vec2 upper(largerX, largerY);
+   // //
+   // //   b2AABB aabb;
+   // //   aabb.lowerBound = lower;
+   // //   aabb.upperBound = upper;
+   //
+   // // query nearby region
+   // PlayerAABBQueryCallback queryCallback;
+   //
+   // // player aabb is: 19.504843, 158.254532 to 19.824877 158.966980
+   // // x: 0.320034 * 0.5 -> 0.1600171
+   // // y: 0.712448 * 0.5 -> 0.356224
+   // b2AABB aabb;
+   // float w = 0.1600171f;
+   // float h = 0.356224f;
+   // b2Vec2 center = player_body->GetWorldCenter();
+   // aabb.lowerBound = b2Vec2(center.x - w, center.y - h);
+   // aabb.upperBound = b2Vec2(center.x + w, center.y + h);
+   //
+   // //   b2AABB aabb;
+   // //   aabb.lowerBound = b2Vec2(FLT_MAX,FLT_MAX);
+   // //   aabb.upperBound = b2Vec2(-FLT_MAX,-FLT_MAX);
+   // //   auto fixture = mBody->GetFixtureList();
+   // //   while (fixture)
+   // //   {
+   // //       aabb.Combine(aabb, fixture->GetAABB(0));
+   // //       fixture = fixture->GetNext();
+   // //   }
+   //
+   // // printf("player aabb is: %f, %f to %f %f\n", aabb.lowerBound.x, aabb.lowerBound.y, aabb.upperBound.x, aabb.upperBound.y);
+   //
+   // player_body->GetWorld()->QueryAABB(&queryCallback, aabb);
+   //
+   // // std::remove_if(queryCallback.foundBodies.begin(), queryCallback.foundBodies.end(), [this](b2Body* body){return body == mBody;});
+   // queryCallback._bodies.erase(player_body);
+   //
+   // // printf("bodies in range:\n");
+   // for (auto body : queryCallback._bodies)
+   // {
+   //    if (body->GetType() == b2_staticBody)
+   //    {
+   //       // printf("- static body: %p\n", body);
+   //
+   //       auto found = false;
+   //       auto dist_max = 0.16f;
+   //       auto fixture = body->GetFixtureList();
+   //
+   //       while (fixture)
+   //       {
+   //          auto shape = dynamic_cast<b2ChainShape*>(fixture->GetShape());
+   //
+   //          if (shape)
+   //          {
+   //             b2Vec2 closest;
+   //
+   //             auto edge_length_minimum = 1000.0f;
+   //             for (auto index = 0; index < shape->m_count; index++)
+   //             {
+   //                auto curr = shape->m_vertices[index];
+   //                auto edge_length_squared = (aabb.GetCenter() - curr).LengthSquared();
+   //
+   //                if (edge_length_squared >= dist_max)
+   //                {
+   //                   continue;
+   //                }
+   //
+   //                // does player point to edge direction?
+   //                if (!edgeMatchesMovement(aabb.GetCenter() - curr))
+   //                {
+   //                   continue;
+   //                }
+   //
+   //                // joint in spe needs to point up, since we're holding somewhere4
+   //                auto joint_dir = (aabb.GetCenter() - curr);
+   //                if (joint_dir.y <= 0.0f)
+   //                {
+   //                   continue;
+   //                }
+   //
+   //                // is the analyzed edge actually climbable?
+   //                if (!isClimbableEdge(shape, index))
+   //                {
+   //                   continue;
+   //                }
+   //
+   //                // printf("joint dir: %f \n", jointDir.y);
+   //
+   //                // if all that is the case, we have a joint
+   //                if (edge_length_squared < edge_length_minimum)
+   //                {
+   //                   edge_length_minimum = edge_length_squared;
+   //                   closest = curr;
+   //                   found = true;
+   //                }
+   //             }
+   //
+   //             // printf("    - closest vtx: %f, %f, distance: %f\n", closest.x, closest.y, distMin);
+   //
+   //             // situation a)
+   //             //
+   //             //    v
+   //             //    +------+ prev/next, same y, greater x
+   //             //    |
+   //             //    |
+   //             //    +
+   //             //  prev/next
+   //             //
+   //             //  same x, greater y
+   //             //
+   //             //
+   //             // situation b)
+   //             //                                        v
+   //             //    prev/next, same y, smaller x +------+
+   //             //                                        |
+   //             //                                        |
+   //             //                                        +
+   //             //                                      prev/next
+   //             //
+   //             //                                      same x, greater y
+   //             //
+   //             //
+   //             // prev or current needs to have greater y
+   //             // other vertex must have different x
+   //
+   //             if (found)
+   //             {
+   //                b2DistanceJointDef joint_def;
+   //                joint_def.Initialize(player_body, body, aabb.GetCenter(), closest);
+   //                joint_def.collideConnected = true;
+   //                // jointDefinition.dampingRatio = 0.5f;
+   //                // jointDefinition.frequencyHz = 5.0f;
+   //                // jointDefinition.length = 0.01f;
+   //
+   //                Audio::getInstance().playSample({"impact.wav"});
+   //                _climb_joint = player_body->GetWorld()->CreateJoint(&joint_def);
+   //             }
+   //
+   //             // no need to continue processing
+   //             break;
+   //          }
+   //
+   //          fixture = fixture->GetNext();
+   //       }
+   //    }
+   // }
+   //
+   // // mDistanceJoint->SetLength(distanceJoint.GetLength() * 0.99f);
 }
 
 //----------------------------------------------------------------------------------------------------------------------
