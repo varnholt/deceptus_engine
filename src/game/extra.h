cc5f2f4024e3b2a1de53b914b546b8d905a13143
Matthias Varnholt <matthias.varnholt@gmail.com>
2023-08-31 21:27:00 +0200

    extras should no longer be tile-based

--- src/game/extra.h
+++ src/game/extra.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <functional>
 #include <memory>
 #include <vector>
 
@@ -15,40 +16,31 @@ class TileMap;
 struct TmxLayer;
 struct TmxTileSet;
 
-class Extra
+class Extra : public GameNode
 {
 public:
    struct ExtraItem : public GameNode
    {
-      enum class ExtraSpriteIndex
-      {
-         Coin = 0,
-         Cherry = 16,
-         Banana = 32,
-         Apple = 48,
-         KeyRed = 64,
-         KeyOrange = 65,
-         KeyBlue = 66,
-         KeyGreen = 67,
-         KeyYellow = 68,
-         Dash = 80,
-         Invalid = 0xffff
-      };
 
       ExtraItem(GameNode* parent = nullptr);
 
       bool _active = true;
-      sf::Vector2u _sprite_offset;
-      sf::Vector2f _position;
-      ExtraSpriteIndex _type = ExtraSpriteIndex::Invalid;
+      std::string _name;
+      std::string _sample;
+      sf::Sprite _sprite;
+      std::shared_ptr<sf::Texture> _texture;
+      sf::FloatRect _rect;
+      int32_t _z = 0;
    };
+
+   using ExtraCollback = std::function<void(const std::string&)>;
 
    void deserialize(GameNode* parent, const GameDeserializeData& data);
    void collide(const sf::FloatRect& player_rect);
    void resetExtras();
 
-   Extra() = default;
+   Extra(GameNode* parent = nullptr);
 
    std::vector<std::shared_ptr<ExtraItem>> _extra_items;
-   std::shared_ptr<TileMap> _tilemap;
+   std::vector<ExtraCollback> _callbacks;
 };
