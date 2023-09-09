0967b52af5411b2dbce4d59f208efb945e87da65
Matthias Varnholt <matthias@cercare-medical.com>
2023-09-09 16:08:33 +0200

    fear not! player light can be controlled in tweaks.json

--- src/game/tweaks.cpp
+++ src/game/tweaks.cpp
@@ -23,22 +23,20 @@ const Tweaks& Tweaks::instance()
 
 std::string Tweaks::serialize()
 {
-   nlohmann::json config =
-   {
-      {
-         "Tweaks",
-         {
-            {"bend_down_threshold",    _bend_down_threshold},
-            {"cpan_tolerance_x",       _cpan_tolerance_x},
-            {"cpan_tolerance_y",       _cpan_tolerance_y},
-            {"cpan_max_distance_px",   _cpan_max_distance_px},
-            {"cpan_look_speed_x",      _cpan_look_speed_x},
-            {"cpan_look_speed_y",      _cpan_look_speed_y},
-            {"cpan_snap_back_factor",  _cpan_snap_back_factor},
-            {"enter_portal_threshold", _enter_portal_threshold},
-         }
-      }
-   };
+   nlohmann::json config = {
+      {"Tweaks",
+       {
+          {"bend_down_threshold", _bend_down_threshold},
+          {"cpan_tolerance_x", _cpan_tolerance_x},
+          {"cpan_tolerance_y", _cpan_tolerance_y},
+          {"cpan_max_distance_px", _cpan_max_distance_px},
+          {"cpan_look_speed_x", _cpan_look_speed_x},
+          {"cpan_look_speed_y", _cpan_look_speed_y},
+          {"cpan_snap_back_factor", _cpan_snap_back_factor},
+          {"enter_portal_threshold", _enter_portal_threshold},
+          {"player_light_enabled", _player_light_enabled},
+          {"player_light_alpha", _player_light_alpha},
+       }}};
 
    return toString(config);
 }
@@ -55,5 +53,15 @@ void Tweaks::deserialize(const std::string& data)
    _cpan_look_speed_y      = config["Tweaks"]["cpan_look_speed_y"].get<float>();
    _cpan_snap_back_factor  = config["Tweaks"]["cpan_snap_back_factor"].get<float>();
    _enter_portal_threshold = config["Tweaks"]["enter_portal_threshold"].get<float>();
+
+   if (config.find("player_light_enabled") != config.end())
+   {
+      _player_light_enabled = config.at("player_light_enabled").get<bool>();
+   }
+
+   if (config.find("player_light_alpha") != config.end())
+   {
+      _player_light_alpha = config.at("player_light_alpha").get<uint8_t>();
+   }
 }
 
