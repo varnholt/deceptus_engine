17c6a345492efc0a1e068b87e504835265e1dbea
Matthias Varnholt <matthias.varnholt@gmail.com>
2023-09-30 00:01:25 +0200

    add happy alpha fade

--- src/game/mechanisms/controllerhelp.cpp
+++ src/game/mechanisms/controllerhelp.cpp
@@ -36,7 +36,7 @@ ControllerHelp::ControllerHelp(GameNode* parent)
 
 void ControllerHelp::draw(sf::RenderTarget& target, sf::RenderTarget& /*normal*/)
 {
-   if (!_visible)
+   if (!_visible && _alpha <= 0.01f)
    {
       return;
    }
@@ -45,22 +45,21 @@ void ControllerHelp::draw(sf::RenderTarget& target, sf::RenderTarget& /*normal*/
    //              [xxx]
    //           [xxx] [xxx]
 
+   const auto color = sf::Color(255, 255, 255, static_cast<uint8_t>(_alpha * 255));
    const auto margin_x_px = (_sprites.size() - 1) * PIXELS_PER_TILE / 2.0f;
    const auto width_of_tiles_px = _sprites.size() * PIXELS_PER_TILE + margin_x_px;
    const auto tile_offset_y = sin(_time.asSeconds() * 5.0f) * 8.0f;
 
    // draw background
-   _background.setPosition(
-      _rect_center.x - _background.getTextureRect().width / 2,
-      _rect_center.y + tile_offset_y - 11
-   );
-
+   _background.setPosition(_rect_center.x - _background.getTextureRect().width / 2, _rect_center.y + tile_offset_y - 11);
+   _background.setColor(color);
    target.draw(_background);
 
    // draw icons
    auto index = 0;
    for (auto& sprite : _sprites)
    {
+      sprite.setColor(color);
       const auto tile_offset_x = -width_of_tiles_px / 2.0f + index * PIXELS_PER_TILE * 1.5f;
       sprite.setPosition(_rect_center.x + tile_offset_x, _rect_center.y + tile_offset_y);
       target.draw(sprite);
@@ -68,6 +67,11 @@ void ControllerHelp::draw(sf::RenderTarget& target, sf::RenderTarget& /*normal*/
    }
 }
 
+namespace
+{
+constexpr auto show_speed = 1.0f;
+constexpr auto hide_speed = 1.0f;
+}  // namespace
 
 void ControllerHelp::update(const sf::Time& dt)
 {
@@ -76,10 +80,19 @@ void ControllerHelp::update(const sf::Time& dt)
 
    if (!_visible)
    {
-      return;
-   }
+      _alpha = std::max(0.0f, _alpha - dt.asSeconds() * hide_speed);
 
-   _time += dt;
+      // keep animating until fully disappeared
+      if (_alpha > 0.01f)
+      {
+         _time += dt;
+      }
+   }
+   else
+   {
+      _alpha = std::min(1.0f, _alpha + dt.asSeconds() * show_speed);
+      _time += dt;
+   }
 }
 
 
@@ -88,17 +101,20 @@ void ControllerHelp::deserialize(const GameDeserializeData& data)
    setObjectId(data._tmx_object->_name);
    setZ(static_cast<int32_t>(ZDepth::Player) - 1);
 
+   // clang-format off
    static const std::array<std::string, 16 * 9> key_map{
-      "key_esc", "key_1", "key_2", "key_3", "key_4", "key_5", "key_6", "key_7", "key_8", "key_9", "key_minus", "key_equals", "key_backspace", "", "", "",
-      "key_tab", "key_q", "key_w", "key_e", "key_r", "key_t", "key_y", "key_u", "key_i", "key_o", "key_p", "key_bracket_l", "key_bracket_r", "", "", "",
-      "key_caps", "key_a", "key_s", "key_d", "key_f", "key_g", "key_h", "key_j", "key_k", "key_l", "key_semicolon", "key_apostrophe", "key_return", "", "", "",
-      "key_shift", "key_0", "key_z", "key_x", "key_c", "key_v", "key_b", "key_n", "key_m", "key_comma", "key_period", "key_question", "key_backslash", "", "", "",
-      "key_ctrl", "key_win", "key_alt", "key_empty", "key_list", "", "", "", "", "key_cursor_l", "key_cursor_u", "key_cursor_d", "key_cursor_r", "", "", "",
-      "bt_a", "bt_b", "bt_x", "bt_y", "bt_list", "bt_menu", "bt_rt", "bt_lt", "bt_lb", "bt_rb", "", "", "", "", "", "",
-      "dpad_u", "dpad_d", "dpad_l", "dpad_r", "bt_u", "bt_d", "bt_l", "bt_r", "bt_1", "bt_2", "bt_3", "bt_4", "bt_5", "bt_6", "bt_7", "bt_8",
-      "bt_r_u", "bt_r_d", "bt_r_l", "bt_r_r", "bt_r_u_d", "bt_r_l_r", "dpad_empty", "bt_0", "bt_9", "bt_10", "bt_11", "bt_12", "bt_13", "bt_14", "bt_15", "bt_16",
-      "bt_l_u", "bt_l_d", "bt_l_l", "bt_l_r", "bt_l_u_d", "bt_l_l_r", "", "", "", "", "", "", "", "", "", ""
+      "key_esc",   "key_1",   "key_2",   "key_3",     "key_4",    "key_5",   "key_6",       "key_7", "key_8", "key_9",        "key_minus",     "key_equals",     "key_backspace", "",     "",       "",
+      "key_tab",   "key_q",   "key_w",   "key_e",     "key_r",    "key_t",   "key_y",       "key_u", "key_i", "key_o",        "key_p",         "key_bracket_l",  "key_bracket_r", "",     "",       "",
+      "key_caps",  "key_a",   "key_s",   "key_d",     "key_f",    "key_g",   "key_h",       "key_j", "key_k", "key_l",        "key_semicolon", "key_apostrophe", "key_return",    "",     "",       "",
+      "key_shift", "key_0",   "key_z",   "key_x",     "key_c",    "key_v",   "key_b",       "key_n", "key_m", "key_comma",    "key_period",    "key_question",   "key_backslash", "",     "",       "",
+      "key_ctrl",  "key_win", "key_alt", "key_empty", "key_list",  "",        "",           "",      "",      "key_cursor_l", "key_cursor_u",  "key_cursor_d",   "key_cursor_r",  "",     "",       "",
+      "bt_a",      "bt_b",    "bt_x",    "bt_y",      "bt_list",  "bt_menu",  "bt_rt",      "bt_lt", "bt_lb", "bt_rb",         "",              "",               "",             "",     "",       "",
+      "dpad_u",    "dpad_d",  "dpad_l",  "dpad_r",    "bt_u",     "bt_d",     "bt_l",       "bt_r",  "bt_1",  "bt_2",          "bt_3",          "bt_4",           "bt_5",        "bt_6",  "bt_7",  "bt_8",
+      "bt_r_u",    "bt_r_d",  "bt_r_l",  "bt_r_r",    "bt_r_u_d", "bt_r_l_r", "dpad_empty", "bt_0",  "bt_9",  "bt_10",         "bt_11",         "bt_12",          "bt_13",       "bt_14", "bt_15", "bt_16",
+      "bt_l_u",    "bt_l_d",  "bt_l_l",  "bt_l_r",    "bt_l_u_d", "bt_l_l_r", "",            "",      "",     "",              "",               "",              "",            "",       "",     ""
    };
+
+   // clang-format on
 
    std::map<std::string, std::string> key_controller_map;
    key_controller_map["key_cursor_u"] = "dpad_u";
