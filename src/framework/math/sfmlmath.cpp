9ab6a65050cca6eb393f19e9504b292425db0d38
Matthias Varnholt <matthias.varnholt@gmail.com>
2023-10-08 20:47:00 +0200

    add function to mix colors

--- src/framework/math/sfmlmath.cpp
+++ src/framework/math/sfmlmath.cpp
@@ -28,3 +28,14 @@ sf::Vector2f SfmlMath::normalize(const sf::Vector2f& v)
    }
 }
 
+sf::Color mixColors(const sf::Color& color_1, const sf::Color& color_2, float ratio)
+{
+   const auto inv_ratio = 1.0f - ratio;
+
+   const auto red = static_cast<uint8_t>(color_1.r * ratio + color_2.r * inv_ratio);
+   const auto green = static_cast<uint8_t>(color_1.g * ratio + color_2.g * inv_ratio);
+   const auto blue = static_cast<uint8_t>(color_1.b * ratio + color_2.b * inv_ratio);
+   const auto alpha = static_cast<uint8_t>(color_1.a * ratio + color_2.a * inv_ratio);
+
+   return sf::Color(red, green, blue, alpha);
+}
