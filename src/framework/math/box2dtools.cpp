2681da7bae026282d0b2add01f479a6b14a43e46
Matthias Varnholt <matthias.varnholt@gmail.com>
2023-10-16 22:29:14 +0200

    moveable box is beveled now

--- src/framework/math/box2dtools.cpp
+++ src/framework/math/box2dtools.cpp
@@ -6,26 +6,29 @@ b2PolygonShape Box2DTools::createBeveledBox(const float width, const float heigh
 {
    auto box_shape = b2PolygonShape();
 
-   // Ensure bevel percentage is within a valid range (0 to 50%)
+   // ensure bevel percentage is within a valid range (0 to 50%)
    const auto clamped_bevel_percentage = std::clamp(bevel_percentage, 0.0f, 0.5f);
 
-   // Calculate the bevel offset based on the bevel percentage
+   // calculate the bevel offset based on the bevel percentage
    const auto bevel_offset = std::min(width, height) * clamped_bevel_percentage;
 
-   // Define the vertices of the beveled box
-   const auto half_width = width * 0.5f;
-   const auto half_height = height * 0.5f;
+   //    0----7
+   //   /      \
+   //  1        6
+   //  |        |
+   //  2        5
+   //   \      /
+   //    3----4
    const auto vertices = std::array<b2Vec2, 8>{
-      b2Vec2(-half_width, -half_height),
-      b2Vec2(half_width, -half_height),
-      b2Vec2(half_width - bevel_offset, -half_height + bevel_offset),
-      b2Vec2(-half_width + bevel_offset, -half_height + bevel_offset),
-      b2Vec2(-half_width, half_height),
-      b2Vec2(half_width, half_height),
-      b2Vec2(half_width - bevel_offset, half_height - bevel_offset),
-      b2Vec2(-half_width + bevel_offset, half_height - bevel_offset)};
+      b2Vec2(bevel_offset, 0),
+      b2Vec2(width - bevel_offset, 0),
+      b2Vec2(width, bevel_offset),
+      b2Vec2(width, height - bevel_offset),
+      b2Vec2(width - bevel_offset, height),
+      b2Vec2(bevel_offset, height),
+      b2Vec2(0, height - bevel_offset),
+      b2Vec2(0, bevel_offset)};
 
-   // Set the vertices in counter-clockwise order
    box_shape.Set(vertices.data(), vertices.size());
 
    return box_shape;
