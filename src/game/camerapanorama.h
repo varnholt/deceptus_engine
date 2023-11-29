41c65ce37ef87b180726f153bc3e5164cb9e1dc4
Matthias Varnholt <matthias.varnholt@gmail.com>
2023-11-29 21:24:56 +0100

    unify cpan code

--- src/game/camerapanorama.h
+++ src/game/camerapanorama.h
@@ -26,6 +26,21 @@ public:
 private:
    CameraPanorama() = default;
 
+   // clang-format off
+   sf::Vector2f computeDesiredLookVector(
+      bool looking_up,
+      bool looking_down,
+      bool looking_left,
+      bool looking_right,
+      bool can_look_up,
+      bool can_look_down,
+      bool can_look_left,
+      bool can_look_right,
+      float x_relative,
+      float y_relative
+   ) const;
+   // clang-format on
+
    int32_t _look_state = static_cast<int32_t>(Look::Inactive);
    sf::Vector2f _look_vector;
 };
