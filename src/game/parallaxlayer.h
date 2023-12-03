ea5fce3e36c413f833110bf7fd6b02ac3a59d0cb
Matthias Varnholt <matthias.varnholt@gmail.com>
2023-12-03 20:59:01 +0100

    add parallax support to image layers

--- src/game/parallaxlayer.h
+++ src/game/parallaxlayer.h
@@ -8,12 +8,14 @@
 // parallax (move to separate mechanism!)
 struct ParallaxLayer
 {
+   static std::unique_ptr<ParallaxLayer> deserialize(const std::shared_ptr<TmxLayer>& layer, const std::shared_ptr<TileMap>& tile_map);
+   void updateView(float level_view_x, float level_view_y, float view_width, float view_height);
+   void resetView(float view_width, float view_height);
+
    int32_t _z_index = 0;
    ParallaxSettings _settings;
    std::shared_ptr<TileMap> _tile_map;
    sf::View _view;
-
-   static std::unique_ptr<ParallaxLayer> deserialize(const std::shared_ptr<TmxLayer>& layer, const std::shared_ptr<TileMap>& tile_map);
 };
 
 #endif  // PARALLAXLAYER_H
