6f256806d08fe10fe3f58507f872855222c8126d
Matthias Varnholt <matthias.varnholt@gmail.com>
2023-12-17 14:17:23 +0100

    added new class 'inventoryimages'

---
+++ src/game/inventoryimages.cpp
@@ -0,0 +1,62 @@
+#include "inventoryimages.h"
+
+#include "framework/tools/log.h"
+
+#include <filesystem>
+#include <fstream>
+
+using json = nlohmann::json;
+
+void InventoryImages::to_json(nlohmann::json& j, const InventoryImages::InventoryImage& image)
+{
+   j = json{
+      {"x_px", image._x_px},
+      {"y_px", image._y_px},
+      {"width_px", image._width_px},
+      {"height_px", image._height_px},
+   };
+}
+
+void InventoryImages::from_json(const json& j, InventoryImages::InventoryImage& image)
+{
+   j.at("x_px").get_to(image._x_px);
+   j.at("y_px").get_to(image._y_px);
+   j.at("width_px").get_to(image._width_px);
+   j.at("height_px").get_to(image._height_px);
+}
+
+std::vector<InventoryImages::InventoryImage> InventoryImages::readImages()
+{
+   const auto path = "data/sprites/inventory_items.json";
+
+   if (!std::filesystem::exists(path))
+   {
+      return {};
+   }
+
+   std::ifstream ifs(path, std::ifstream::in);
+
+   auto c = ifs.get();
+   std::string data;
+
+   while (ifs.good())
+   {
+      data.push_back(static_cast<char>(c));
+      c = ifs.get();
+   }
+
+   ifs.close();
+
+   try
+   {
+      const json json_data = json::parse(data);
+      std::vector<InventoryImage> images = json_data;
+      return images;
+   }
+   catch (const std::exception& e)
+   {
+      Log::Error() << e.what();
+   }
+
+   return {};
+}
