dc33df0962edcd3b1117cafee552725965db7eaa
Matthias Varnholt <matthias.varnholt@gmail.com>
2024-03-09 22:46:17 +0100

    image layer is now a gamemechanism

--- src/game/imagelayer.h
+++ src/game/imagelayer.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "game/gamemechanism.h"
+#include "game/gamenode.h"
 #include "parallaxsettings.h"
 
 #include <SFML/Graphics.hpp>
@@ -8,17 +10,24 @@
 
 struct TmxElement;
 
-struct ImageLayer
+class ImageLayer : public GameMechanism, public GameNode
 {
+public:
+   ImageLayer(GameNode* parent = nullptr);
+
+   void draw(sf::RenderTarget& target, sf::RenderTarget& normal) override;
+
    void updateView(float level_view_x, float level_view_y, float view_width, float view_height);
    void resetView(float view_width, float view_height);
-   void draw(sf::RenderTarget& target, sf::RenderTarget& normal);
+
+   std::optional<sf::FloatRect> getBoundingBoxPx() override;
+
    static std::shared_ptr<ImageLayer> deserialize(const std::shared_ptr<TmxElement>& element, const std::filesystem::path& level_path);
 
+private:
    sf::Sprite _sprite;
    std::shared_ptr<sf::Texture> _texture;
    sf::BlendMode _blend_mode = sf::BlendAlpha;
-   int32_t _z_index = 0;
 
    sf::View _parallax_view;
    std::optional<ParallaxSettings> _parallax_settings;
