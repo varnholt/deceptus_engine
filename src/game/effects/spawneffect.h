5ed38ebe96bd7394136b069977d1d235110995fe
Matthias Varnholt <matthias.varnholt@gmail.com>
2024-03-20 22:43:08 +0100

    prepare spawn effect code

--- src/game/effects/spawneffect.h
+++ src/game/effects/spawneffect.h
@@ -3,6 +3,8 @@
 
 #include <SFML/Graphics.hpp>
 
+#include "constants.h"
+
 class SpawnEffect
 {
 public:
@@ -13,6 +15,30 @@ public:
 
 private:
    sf::Time _elapsed;
+
+   struct Particle
+   {
+      sf::Vector2f _pos_px;
+      sf::Time _elapsed;
+      float _velocity{0.0f};
+      float _radius_px{60.0f};
+      sf::Vector2f _offset_px;
+
+      float _alpha_all_particles{1.0f};
+      float _alpha_per_particle{1.0f};
+
+      sf::Sprite _sprite;
+
+      void spawn();
+      void update(const sf::Time& dt);
+   };
+
+   void createParticles(const sf::Vector2f& offset_px, int32_t count, float radius_px, const std::shared_ptr<sf::Texture>& texture);
+   void drawParticles(sf::RenderTarget& target);
+   void updateParticles(const sf::Time& dt);
+
+   std::vector<Particle> _particles;
+   std::shared_ptr<sf::Texture> _texture;
 };
 
 #endif  // SPAWNEFFECT_H
