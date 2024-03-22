1535998c4bdfe50f4c0ad96ba7213f9a5b850f80
Matthias Varnholt <matthias.varnholt@gmail.com>
2024-03-22 23:57:33 +0100

    continued spawn effect

--- src/game/effects/spawneffect.cpp
+++ src/game/effects/spawneffect.cpp
@@ -1,48 +1,57 @@
 #include "spawneffect.h"
 
+#include "game/animationpool.h"
+#include "game/constants.h"
 #include "game/texturepool.h"
 
-SpawnEffect::SpawnEffect()
+SpawnEffect::SpawnEffect(const sf::Vector2f pos_px)
 {
-   _texture = TexturePool::getInstance().get("data/effects/spawn_particles.png");
+   _particles = std::make_unique<ParticleEffect>(pos_px, 50, 100);
 }
 
-void SpawnEffect::draw(sf::RenderTarget& target, sf::RenderTarget& /*normal*/)
+void SpawnEffect::draw(sf::RenderTarget& target)
 {
-   drawParticles(target);
+   _particles->draw(target);
 }
 
 void SpawnEffect::update(const sf::Time& dt)
 {
-   _elapsed += dt;
-   updateParticles(dt);
+   _particles->update(dt);
+   _orb->update(dt);
 }
 
-void SpawnEffect::createParticles(
-   const sf::Vector2f& offset_px,
-   int32_t count,
-   float radius_px,
-   const std::shared_ptr<sf::Texture>& texture
-)
+bool SpawnEffect::isFinished() const
 {
+   return _orb->_animation_hide->_finished;
+}
+
+SpawnEffect::ParticleEffect::ParticleEffect(const sf::Vector2f& offset_px, int32_t count, float radius_px)
+{
+   // texture: 3000 x 300
+   // row 1: 30 * 50 x 50 (show)
+   // row 2: 60 * 50 x 50 (idle)
+   // row 3: 12 * 100 x 100 (hide)
+   _texture = TexturePool::getInstance().get("data/effects/spawn_particles.png");
+
    _particles.reserve(count);
    for (auto i = 0; i < count; i++)
    {
       Particle particle;
       particle._offset_px = offset_px;
       particle._radius_px = radius_px;
-      particle._sprite.setTexture(*texture);
+      particle._sprite.setTexture(*_texture);
       particle.spawn();
+
       _particles.push_back(particle);
    }
 }
 
-void SpawnEffect::drawParticles(sf::RenderTarget& target)
+void SpawnEffect::ParticleEffect::draw(sf::RenderTarget& target)
 {
    std::ranges::for_each(_particles, [&target](const auto& particle) { target.draw(particle._sprite); });
 }
 
-void SpawnEffect::updateParticles(const sf::Time& dt)
+void SpawnEffect::ParticleEffect::update(const sf::Time& dt)
 {
    std::ranges::for_each(_particles, [&dt](auto& particle) { particle.update(dt); });
 }
@@ -84,4 +93,61 @@ void SpawnEffect::Particle::update(const sf::Time& dt)
 
    // alpha should increase within the first second after spawn or so
    // also alpha should have a static alpha so the overall effect can be activated and deactivated
+}
+
+SpawnEffect::Orb::Orb(const sf::Vector2f& pos_px)
+{
+   _texture = TexturePool::getInstance().get("data/effects/spawn_orb.png");
+
+   AnimationPool animation_pool{"data/effects/spawn_effect_animations.json"};
+
+   _animation_show = animation_pool.create("show", pos_px.x, pos_px.y, false, false);
+   _animation_idle = animation_pool.create("idle", pos_px.x, pos_px.y, false, false);
+   _animation_hide = animation_pool.create("hide", pos_px.x, pos_px.y, false, false);
+
+   _animation_show->play();
+}
+
+void SpawnEffect::Orb::draw(sf::RenderTarget& target)
+{
+   if (!_animation_show->_paused)
+   {
+      _animation_show->draw(target);
+   }
+   else if (!_animation_idle->_paused)
+   {
+      _animation_idle->draw(target);
+   }
+   else if (!_animation_hide->_paused)
+   {
+      _animation_hide->draw(target);
+   }
+}
+
+void SpawnEffect::Orb::update(const sf::Time& dt)
+{
+   if (!_animation_show->_paused)
+   {
+      _animation_show->update(dt);
+   }
+
+   if (_animation_show->_finished)
+   {
+      _animation_idle->play();
+   }
+
+   if (!_animation_idle->_paused)
+   {
+      _animation_idle->update(dt);
+   }
+
+   if (_animation_idle->_finished)
+   {
+      _animation_hide->play();
+   }
+
+   if (!_animation_hide->_paused)
+   {
+      _animation_hide->update(dt);
+   }
 }
