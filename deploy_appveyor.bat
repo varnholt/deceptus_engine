d8f075f8e5cea0724678ffccc8326e158bd68f05
Matthias Varnholt <matthias@cercare-medical.com>
2024-07-08 16:00:07 +0200

    fix build

--- deploy_appveyor.bat
+++ deploy_appveyor.bat
@@ -1,13 +1,13 @@
-IF NOT EXIST build mkdir build
-IF NOT EXIST build\data mkdir build\data
-IF NOT EXIST build\tools mkdir build\tools
+IF NOT EXIST artifacts mkdir artifacts
+IF NOT EXIST artifacts\data mkdir artifacts\data
+IF NOT EXIST artifacts\tools mkdir artifacts\tools
 
-xcopy /Y thirdparty\lua\lib64\*.dll build
-xcopy /Y thirdparty\sfml\bin\*.dll build
-xcopy /Y thirdparty\sdl\lib\x64\*.dll build
-xcopy /E /Y data build\data
-xcopy /E /Y tools build\tools
-xcopy /E /Y tools\level_selector\level_selector.exe build
-xcopy /Y deceptus.exe build
+xcopy /Y thirdparty\lua\lib64\*.dll artifacts
+xcopy /Y thirdparty\sfml\bin\*.dll artifacts
+xcopy /Y thirdparty\sdl\lib\x64\*.dll artifacts
+xcopy /E /Y data artifacts\data
+xcopy /E /Y tools artifacts\tools
+xcopy /E /Y tools\level_selector\level_selector.exe artifacts
+xcopy /Y build\Release\deceptus.exe artifacts
 
-IF EXIST deceptus.pdb xcopy /C /Y deceptus.pdb build
+IF EXIST build\Release\deceptus.pdb xcopy /C /Y build\Release\deceptus.pdb artifacts
