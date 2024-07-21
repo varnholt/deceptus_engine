837c9b00465c8a8e2645857fffb56ecfe0d84b61
Matthias Varnholt <matthias.varnholt@gmail.com>
2024-07-21 16:13:00 +0200

    add a bunch of error handling, fallbacks

--- src/game/layers/ambientocclusion.cpp
+++ src/game/layers/ambientocclusion.cpp
@@ -12,43 +12,8 @@
 
 void AmbientOcclusion::load(const std::filesystem::path& path, const std::string& base_filename)
 {
-   // read config file
-   const auto config_filename = path / "ambient_occlusion.json";
-   std::ifstream ifs(config_filename, std::ifstream::in);
-   auto c = static_cast<char>(ifs.get());
-   std::string data;
-
-   while (ifs.good())
-   {
-      data.push_back(c);
-      c = static_cast<char>(ifs.get());
-   }
-
-   ifs.close();
-
-   // parse json
-   nlohmann::json json_config;
-   try
-   {
-      json_config = nlohmann::json::parse(data);
-   }
-   catch (const std::exception& e)
-   {
-      std::cerr << e.what() << std::endl;
-   }
-
-   // read config from json
-   _config = json_config.get<Config>();
-   const auto texture = (path / _config._texture_filename).string();
-   const auto uv = (path / _config._uv_filename).string();
-
-   if (!std::filesystem::exists(texture))
-   {
-      Log::Error() << "need to create an ambient occlusion map (" << texture << ")";
-      return;
-   }
-
-   _texture = TexturePool::getInstance().get(texture);
+   _config = Config(path, base_filename);
+   _texture = TexturePool::getInstance().get(_config._texture_filename);
 
    auto xi = 0;
    auto yi = 0;
@@ -62,37 +27,35 @@ void AmbientOcclusion::load(const std::filesystem::path& path, const std::string
    auto group_y = 0;
 
    std::string line;
-   std::ifstream uv_file(uv);
-   if (uv_file.is_open())
+   std::ifstream uv_file(_config._uv_filename);
+   if (!uv_file.is_open())
    {
-      while (uv_file.good())
+      return;
+   }
+
+   while (uv_file.good())
+   {
+      std::getline(uv_file, line);
+      std::sscanf(line.c_str(), "%d;%d;%d;%d;%d", &i, &x, &y, &w, &h);
+
+      sf::Sprite sprite;
+      sprite.setPosition(static_cast<float>(x - 5), static_cast<float>(y - 6));
+      sprite.setTexture(*_texture);
+      sprite.setTextureRect({xi, yi, w, h});
+
+      group_x = (x >> 8);
+      group_y = (y >> 8);
+      _sprite_map[group_y][group_x].push_back(sprite);
+
+      xi += w;
+      if (xi == static_cast<int32_t>(_texture->getSize().x))
       {
-         std::getline(uv_file, line);
-         std::sscanf(line.c_str(), "%d;%d;%d;%d;%d", &i, &x, &y, &w, &h);
-
-         sf::Sprite sprite;
-         sprite.setPosition(static_cast<float>(x - 5), static_cast<float>(y - 6));
-         sprite.setTexture(*_texture);
-         sprite.setTextureRect({xi, yi, w, h});
-
-         group_x = (x >> 8);
-         group_y = (y >> 8);
-         _sprite_map[group_y][group_x].push_back(sprite);
-
-         xi += w;
-         if (xi == static_cast<int32_t>(_texture->getSize().x))
-         {
-            xi = 0;
-            yi += h;
-         }
+         xi = 0;
+         yi += h;
       }
+   }
 
-      uv_file.close();
-   }
-   else
-   {
-      Log::Error() << "AmbientOcclusion::load: unable to open uv file: " << uv;
-   }
+   uv_file.close();
 }
 
 void AmbientOcclusion::draw(sf::RenderTarget& window)
@@ -140,9 +103,68 @@ int32_t AmbientOcclusion::getZ() const
    return _config._z_index;
 }
 
-void from_json(const nlohmann::json& j, AmbientOcclusion::Config& settings)
+AmbientOcclusion::Config::Config(const std::filesystem::path& path, const std::string& base_filename)
+    : _path(path), _base_filename(base_filename)
 {
-   settings._texture_filename = j.at("texture_filename").get<std::string>();
-   settings._uv_filename = j.at("uv_filename").get<std::string>();
-   settings._z_index = j.at("z_index").get<int32_t>();
+   // read config file
+   const auto config_filename = path / "ambient_occlusion.json";
+   std::ifstream ifs(config_filename, std::ifstream::in);
+   auto c = static_cast<char>(ifs.get());
+   std::string data;
+
+   while (ifs.good())
+   {
+      data.push_back(c);
+      c = static_cast<char>(ifs.get());
+   }
+
+   ifs.close();
+
+   // parse json
+   nlohmann::json j;
+   try
+   {
+      j = nlohmann::json::parse(data);
+   }
+   catch (const std::exception& e)
+   {
+      std::cerr << e.what() << std::endl;
+   }
+
+   if (j.find("texture_filename") != j.end())
+   {
+      _texture_filename = (path / j.at("texture_filename").get<std::string>()).string();
+   }
+   else
+   {
+      _texture_filename = (path / _base_filename).string();
+   }
+
+   if (j.find("uv_filename") != j.end())
+   {
+      _uv_filename = (path / j.at("uv_filename").get<std::string>()).string();
+   }
+   else
+   {
+      _uv_filename = (path / (base_filename + "_ao_tiles.uv")).string();
+   }
+
+   if (j.find("z_index") != j.end())
+   {
+      _z_index = j.at("z_index").get<int32_t>();
+   }
+   else
+   {
+      _z_index = static_cast<int32_t>(ZDepth::Player);
+   }
+
+   if (!std::filesystem::exists(_texture_filename))
+   {
+      Log::Error() << "ambient occlusion texture not found (" << _texture_filename << ")";
+   }
+
+   if (!std::filesystem::exists(_uv_filename))
+   {
+      Log::Error() << "ambient occlusion UV file not found (" << _uv_filename << ")";
+   }
 }
