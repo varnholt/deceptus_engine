fc97328c86287319a09c8fd2863624703c1f9f36
Matthias Varnholt <matthias@cercare-medical.com>
2024-07-26 13:39:55 +0200

    finished packtexture overhaul

--- lab/packtexture/texturelabel.h
+++ lab/packtexture/texturelabel.h
@@ -7,13 +7,12 @@
 class TextureLabel : public QLabel
 {
 public:
+   TextureLabel(QWidget* p = nullptr);
+
    QPixmap _pixmap;
    std::map<uint32_t, Quad>* _quads = nullptr;
    float _scale_x = 1.0f;
    float _scale_y = 1.0f;
-
-public:
-   TextureLabel(QWidget* p = nullptr);
 
 protected:
    void paintEvent(QPaintEvent*);
