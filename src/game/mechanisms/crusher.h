154683fb6b37700c29fafb74c457b74b565a2c22
Matthias Varnholt <matthias.varnholt@gmail.com>
2024-07-30 23:27:20 +0200

    fix crushers for dstar's new layout

--- src/game/mechanisms/crusher.h
+++ src/game/mechanisms/crusher.h
@@ -14,63 +14,62 @@ struct TmxObject;
 
 class Crusher : public GameMechanism, public GameNode
 {
-   public:
+public:
+   enum class Mode
+   {
+      Interval,
+      Distance
+   };
 
-      enum class Mode
-      {
-         Interval,
-         Distance
-      };
+   enum class State
+   {
+      Idle,
+      Extract,
+      Retract
+   };
 
-      enum class State
-      {
-         Idle,
-         Extract,
-         Retract
-      };
+   Crusher(GameNode* parent = nullptr);
 
-      Crusher(GameNode* parent = nullptr);
+   void draw(sf::RenderTarget& color, sf::RenderTarget& normal) override;
+   void update(const sf::Time& dt) override;
+   std::optional<sf::FloatRect> getBoundingBoxPx() override;
 
-      void draw(sf::RenderTarget& color, sf::RenderTarget& normal) override;
-      void update(const sf::Time& dt) override;
-      std::optional<sf::FloatRect> getBoundingBoxPx() override;
+   void setup(const GameDeserializeData& data);
 
-      void setup(const GameDeserializeData& data);
+private:
+   void updateTransform();
+   void setupBody(const std::shared_ptr<b2World>& world);
 
+   void step(const sf::Time& dt);
+   void updateState();
+   void updateSpritePositions();
 
-   private:
+   Mode _mode = Mode::Interval;
+   State _state = State::Idle;
+   State _state_previous = State::Idle;
+   Alignment _alignment = Alignment::PointsDown;
 
-      void updateTransform();
-      void setupBody(const std::shared_ptr<b2World>& world);
+   b2Body* _body{nullptr};
+   sf::Vector2f _pixel_position;
+   sf::Vector2f _blade_offset;
+   sf::FloatRect _rect;
 
-      void step(const sf::Time& dt);
-      void updateState();
-      void updateSpritePositions();
+   sf::Time _idle_time;
+   sf::Time _extraction_time;
+   sf::Time _retraction_time;
 
-      Mode _mode = Mode::Interval;
-      State _state = State::Idle;
-      State _state_previous = State::Idle;
-      Alignment _alignment = Alignment::PointsDown;
+   sf::Sprite _sprite_spike;
+   sf::Sprite _sprite_pusher;
+   sf::Sprite _sprite_mount;
+   sf::Vector2f _pixel_offset_mount;
+   sf::Vector2f _pixel_offset_pusher;
+   sf::Vector2f _pixel_offset_spike;
 
-      b2Body* _body = nullptr;
-      sf::Vector2f _pixel_position;
-      sf::Vector2f _blade_offset;
-      sf::FloatRect _rect;
+   bool _shake{true};
+   bool _shake_shown{false};
 
-      sf::Time _idle_time;
-      sf::Time _extraction_time;
-      sf::Time _retraction_time;
+   int32_t _instance_id{0};
 
-      sf::Sprite _sprite_spike;
-      sf::Sprite _sprite_pusher;
-      sf::Sprite _sprite_mount;
-      sf::Vector2f _pixel_offset_mount;
-      sf::Vector2f _pixel_offset_pusher;
-      sf::Vector2f _pixel_offset_spike;
-
-      int32_t _instance_id = 0;
-
-      std::shared_ptr<sf::Texture> _texture;
-      static int32_t __instance_counter;
+   std::shared_ptr<sf::Texture> _texture;
+   static int32_t __instance_counter;
 };
-
