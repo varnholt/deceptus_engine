19f115c486ea0221a119c6417e57f44c8d42d851
Matthias Varnholt <matthias@cercare-medical.com>
2024-08-27 14:24:42 +0200

    update to new spriteset

--- src/game/mechanisms/deathblock.cpp
+++ src/game/mechanisms/deathblock.cpp
@@ -9,7 +9,22 @@
 #include "game/level/fixturenode.h"
 #include "game/player/player.h"
 
-#define DEBUG_DRAW 1
+// state         frames
+// spikes-open : 0,1,2,3,4,5,6,7,8,9,10,11 (speed 50)
+// idle        : 12 (no speed)
+// close       : 13,14,15,16,17,18,19 (speed 50)
+
+namespace
+{
+//      0: retracted (spikes in)
+//  1..12: extract..fully extracted (spikes out)
+// 13..19: retract..almost retracted (spikes in), requires change to 0 afterwards
+
+constexpr auto sprite_extracted = 12;
+constexpr auto sprite_retracted = 19;
+}  // namespace
+
+// #define DEBUG_DRAW 1
 
 DeathBlock::DeathBlock(GameNode* parent) : GameNode(parent)
 {
@@ -18,50 +33,17 @@ DeathBlock::DeathBlock(GameNode* parent) : GameNode(parent)
 
 void DeathBlock::draw(sf::RenderTarget& color, sf::RenderTarget& /*normal*/)
 {
-   for (auto& sprite : _sprites)
+   for (auto& spike : _spikes)
    {
-      color.draw(sprite);
-   }
+      color.draw(spike._sprite);
 
 #ifdef DEBUG_DRAW
-   const auto x_px = static_cast<int32_t>(_body->GetPosition().x * PPM - PIXELS_PER_TILE);
-   const auto y_px = static_cast<int32_t>(_body->GetPosition().y * PPM - PIXELS_PER_TILE);
-   for (auto rect : _collision_rects)
-   {
-      rect.left += x_px;
-      rect.top += y_px;
-      DebugDraw::drawRect(color, rect);
-   }
+      DebugDraw::drawRect(color, spike._collision_rect_absolute);
 #endif
+   }
+
+   color.draw(_center_sprite);
 }
-
-// enemy_deathblock
-// 24 animation cycles
-
-//      0: retracted (spikes in)
-//  1..19: extract..fully extracted (spikes out)
-// 20..24: retract..almost retracted (spikes in), requires change to 0 afterwards
-
-// 0: spikes out
-// 13: spikes in
-//
-// sprite setup:
-//
-//           +---+
-//           | 0 |
-//       +---+---+---+
-//       | 1 | 2 | 3 |
-//       +---+---+---+
-//           | 4 |
-//           +---+
-//
-// offsets:
-//
-//    0: 1, 0
-//    1: 0, 1
-//    2: 1, 1
-//    3: 2, 1
-//    4: 1, 2
 
 void DeathBlock::setupTransform()
 {
@@ -72,8 +54,8 @@ void DeathBlock::setupTransform()
 
 void DeathBlock::setupBody(const std::shared_ptr<b2World>& world)
 {
-   auto size_x_m = PIXELS_PER_TILE / PPM;
-   auto size_y_m = PIXELS_PER_TILE / PPM;
+   const auto size_x_m = PIXELS_PER_TILE / PPM;
+   const auto size_y_m = PIXELS_PER_TILE / PPM;
 
    b2Vec2 vertices[4];
    vertices[0] = b2Vec2(0, 0);
@@ -130,16 +112,83 @@ void DeathBlock::updateCollision()
    const auto y_px = static_cast<int32_t>(_body->GetPosition().y * PPM - PIXELS_PER_TILE);
 
    // want a copy of the original rect
-   for (auto rect : _collision_rects)
+   int32_t index = 0;
+   for (auto& spike : _spikes)
    {
-      rect.left += x_px;
-      rect.top += y_px;
+      spike._collision_rect_absolute.left = spike._collision_rect_relative.left + x_px;
+      spike._collision_rect_absolute.top = spike._collision_rect_relative.top + y_px;
 
-      if (player_rect.intersects(rect))
+      const auto deadly = (spike._state == Spike::State::Extracted);
+
+      if (player_rect.intersects(spike._collision_rect_absolute) && deadly)
       {
-         // check the 4 sub-bounding boxes here and check if the spikes are actually extraced
-
          Player::getCurrent()->damage(1);
+      }
+
+      ++index;
+   }
+}
+
+void DeathBlock::updateStatesInterval(const sf::Time& dt)
+{
+   constexpr auto animation_speed_factor = 40.0f;
+
+   for (auto& spike : _spikes)
+   {
+      auto& time = spike._wait_offset;
+      if (time.asSeconds() > 0.0f)
+      {
+         time -= dt;
+         continue;
+      }
+
+      switch (spike._state)
+      {
+         case Spike::State::Extracting:
+         {
+            spike._state_time_s += animation_speed_factor * dt.asSeconds();
+
+            if (spike._state_time_s >= sprite_extracted)
+            {
+               spike._wait_time = sf::seconds(0);
+               spike._state = Spike::State::Extracted;
+            }
+
+            break;
+         }
+         case Spike::State::Retracting:
+         {
+            spike._state_time_s += animation_speed_factor * dt.asSeconds();
+
+            if (spike._state_time_s >= sprite_retracted)
+            {
+               spike._state_time_s = 0;  // reset after one cycle
+               spike._wait_time = sf::seconds(0);
+               spike._state = Spike::State::Retracted;
+            }
+
+            break;
+         }
+         case Spike::State::Extracted:
+         {
+            spike._wait_time += dt;
+            if (spike._wait_time > _time_on)
+            {
+               spike._state = Spike::State::Retracting;
+            };
+
+            break;
+         }
+         case Spike::State::Retracted:
+         {
+            spike._wait_time += dt;
+            if (spike._wait_time > _time_off)
+            {
+               spike._state = Spike::State::Extracting;
+            };
+
+            break;
+         }
       }
    }
 }
@@ -150,71 +199,15 @@ void DeathBlock::updateStates(const sf::Time& dt)
    {
       case Mode::AlwaysOn:
       {
-         _sprite_indices[SpikeOrientation::Up] = 19;
-         _sprite_indices[SpikeOrientation::Right] = 19;
-         _sprite_indices[SpikeOrientation::Down] = 19;
-         _sprite_indices[SpikeOrientation::Left] = 19;
+         for (auto& spike : _spikes)
+         {
+            spike._sprite_index = sprite_extracted;
+         }
          break;
       }
       case Mode::Interval:
       {
-         for (int32_t index = 0; index < _states.size(); ++index)
-         {
-            const auto state = _states[index];
-            switch (state)
-            {
-               case State::Extracting:
-               {
-                  _state_times[index] += dt.asSeconds();
-
-                  if (_state_times[index] > 18)
-                  {
-                     _state_times[index] = 0;
-                     _states[index] = State::Extracted;
-                  }
-
-                  break;
-               }
-               case State::Retracting:
-               {
-                  _state_times[index] += dt.asSeconds();
-
-                  if (_state_times[index] > 25)
-                  {
-                     _state_times[index] = 0;
-                     _states[index] = State::Retracted;
-                  }
-                  break;
-               }
-               case State::Extracted:
-               {
-                  auto& time = _times[index];
-                  time += dt;
-
-                  if (time > sf::seconds(1.0f))
-                  {
-                     _times[index] = sf::seconds(0.0f);
-                     _states[index] = State::Retracting;
-                  };
-
-                  break;
-               }
-               case State::Retracted:
-               {
-                  auto& time = _times[index];
-                  time += dt;
-
-                  if (time > sf::seconds(1.0f))
-                  {
-                     _times[index] = sf::seconds(0.0f);
-                     _states[index] = State::Extracting;
-                  };
-
-                  break;
-               }
-            }
-         }
-
+         updateStatesInterval(dt);
          break;
       }
       case Mode::Invalid:
@@ -225,19 +218,21 @@ void DeathBlock::updateStates(const sf::Time& dt)
       }
    }
 
-   _state_times[SpikeOrientation::Center] += dt.asSeconds();
+   // update center
+   _center_sprite_time_s += dt.asSeconds();
+   _center_sprite_index = static_cast<int32_t>(_center_sprite_time_s) % sprite_retracted;
 
-   _sprite_indices[SpikeOrientation::Center] = static_cast<int32_t>(_state_times[SpikeOrientation::Center]) % 6;
-   _sprite_indices[SpikeOrientation::Up] = static_cast<int32_t>(_state_times[SpikeOrientation::Up]);
-   _sprite_indices[SpikeOrientation::Right] = static_cast<int32_t>(_state_times[SpikeOrientation::Right]);
-   _sprite_indices[SpikeOrientation::Down] = static_cast<int32_t>(_state_times[SpikeOrientation::Down]);
-   _sprite_indices[SpikeOrientation::Left] = static_cast<int32_t>(_state_times[SpikeOrientation::Left]);
+   // update resulting spriteset offsets
+   for (auto& spike : _spikes)
+   {
+      spike.updateIndex();
+   }
 }
 
 void DeathBlock::updateBoundingBox()
 {
-   _rect.left = _body->GetPosition().x * PPM - PIXELS_PER_TILE;  // + _offsets[1].x * PIXELS_PER_TILE;
-   _rect.top = _body->GetPosition().x * PPM - PIXELS_PER_TILE;   // + _offsets[0].y * PIXELS_PER_TILE;
+   _rect.left = _body->GetPosition().x * PPM - PIXELS_PER_TILE;
+   _rect.top = _body->GetPosition().x * PPM - PIXELS_PER_TILE;
    _rect.width = 3 * PIXELS_PER_TILE;
    _rect.height = 3 * PIXELS_PER_TILE;
 }
@@ -248,18 +243,20 @@ void DeathBlock::updateSprites()
    const auto y = _body->GetPosition().y * PPM - PIXELS_PER_TILE;
    const auto tl_px = PIXELS_PER_TILE * 3;
 
-   // first row is not used
-   _sprites[SpikeOrientation::Up].setTextureRect(sf::IntRect(_sprite_indices[SpikeOrientation::Up] * tl_px, tl_px, tl_px, tl_px));
-   _sprites[SpikeOrientation::Down].setTextureRect(sf::IntRect(_sprite_indices[SpikeOrientation::Down] * tl_px, tl_px * 2, tl_px, tl_px));
-   _sprites[SpikeOrientation::Right].setTextureRect(sf::IntRect(_sprite_indices[SpikeOrientation::Right] * tl_px, tl_px * 3, tl_px, tl_px));
-   _sprites[SpikeOrientation::Left].setTextureRect(sf::IntRect(_sprite_indices[SpikeOrientation::Left] * tl_px, tl_px * 4, tl_px, tl_px));
-   _sprites[SpikeOrientation::Center].setTextureRect(sf::IntRect(_sprite_indices[SpikeOrientation::Center] * tl_px, tl_px * 5, tl_px, tl_px)
-   );
-
-   for (auto& sprite : _sprites)
+   int32_t row = 1;  // first row is reserved for center
+   for (auto& spike : _spikes)
    {
-      sprite.setPosition(x, y);
+      if (spike.hasChanged())
+      {
+         spike._sprite.setTextureRect(sf::IntRect(spike._sprite_index * tl_px, tl_px * row, tl_px, tl_px));
+      }
+
+      spike._sprite.setPosition(x, y);
+      row++;
    }
+
+   _center_sprite.setTextureRect(sf::IntRect(_center_sprite_index * tl_px, 0, tl_px, tl_px));
+   _center_sprite.setPosition(x, y);
 }
 
 void DeathBlock::updatePosition(const sf::Time& dt)
@@ -294,15 +291,42 @@ void DeathBlock::setup(const GameDeserializeData& data)
 {
    _texture = TexturePool::getInstance().get("data/sprites/enemy_deathblock.png");
 
-   for (auto& sprite : _sprites)
+   for (auto& spike : _spikes)
    {
-      sprite.setTexture(*_texture);
+      spike._sprite.setTexture(*_texture);
    }
+
+   // offsets:
+   //
+   //    up: 1, 0
+   //    right: 2, 1
+   //    down: 1, 2
+   //    left: 0, 1
+
+   _spikes[Spike::Orientation::Up]._collision_rect_relative =
+      sf::IntRect{1 * PIXELS_PER_TILE, 0 * PIXELS_PER_TILE, PIXELS_PER_TILE, PIXELS_PER_TILE};
+   _spikes[Spike::Orientation::Right]._collision_rect_relative =
+      sf::IntRect{2 * PIXELS_PER_TILE, 1 * PIXELS_PER_TILE, PIXELS_PER_TILE, PIXELS_PER_TILE};
+   _spikes[Spike::Orientation::Down]._collision_rect_relative =
+      sf::IntRect{1 * PIXELS_PER_TILE, 2 * PIXELS_PER_TILE, PIXELS_PER_TILE, PIXELS_PER_TILE};
+   _spikes[Spike::Orientation::Left]._collision_rect_relative =
+      sf::IntRect{0 * PIXELS_PER_TILE, 1 * PIXELS_PER_TILE, PIXELS_PER_TILE, PIXELS_PER_TILE};
+
+   _center_sprite.setTexture(*_texture);
 
    setZ(static_cast<int32_t>(ZDepth::ForegroundMin) + 1);
 
    _pixel_positions.x = data._tmx_object->_x_px;
    _pixel_positions.y = data._tmx_object->_y_px;
+
+   _time_off = sf::seconds(2.0f);
+   _time_on = sf::seconds(0.2f);
+
+   // constexpr auto delta_time = 2.0f / 3.0f;
+   // _wait_offsets[0] = sf::seconds(delta_time * 0);
+   // _wait_offsets[1] = sf::seconds(delta_time * 1);
+   // _wait_offsets[2] = sf::seconds(delta_time * 2);
+   // _wait_offsets[3] = sf::seconds(delta_time * 3);
 
    setupBody(data._world);
 
@@ -330,4 +354,6 @@ void DeathBlock::setup(const GameDeserializeData& data)
 
       pos_index++;
    }
+
+   updateSprites();
 }
