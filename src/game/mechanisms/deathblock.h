57fa760cb435fe67a19b59d3f545195c1d13805b
Matthias Varnholt <matthias.varnholt@gmail.com>
2024-08-27 14:24:42 +0200

    update to new spriteset

--- src/game/mechanisms/deathblock.h
+++ src/game/mechanisms/deathblock.h
@@ -20,23 +20,12 @@ class DeathBlock : public GameMechanism, public GameNode
 public:
 
    DeathBlock(GameNode* parent = nullptr);
-
    void draw(sf::RenderTarget& color, sf::RenderTarget& normal) override;
    void update(const sf::Time& dt) override;
    std::optional<sf::FloatRect> getBoundingBoxPx() override;
-
    void setup(const GameDeserializeData& data);
 
 private:
-   enum SpikeOrientation
-   {
-      Up = 0,
-      Right = 1,
-      Down = 2,
-      Left = 3,
-      Center = 4,
-   };
-
    enum class Mode
    {
       Invalid,
@@ -45,12 +34,50 @@ private:
       Interval,
    };
 
-   enum class State
+   struct Spike
    {
-      Extracting,
-      Extracted,
-      Retracting,
-      Retracted
+      enum class State
+      {
+         Extracting,
+         Extracted,
+         Retracting,
+         Retracted,
+      };
+
+      enum Orientation
+      {
+         Up = 0,
+         Down = 1,
+         Left = 2,
+         Right = 3,
+      };
+
+      Spike()
+      {
+         _collision_rect_absolute.width = PIXELS_PER_TILE;
+         _collision_rect_absolute.height = PIXELS_PER_TILE;
+      }
+
+      bool hasChanged() const
+      {
+         return _sprite_index != _sprite_index_prev;
+      }
+
+      void updateIndex()
+      {
+         _sprite_index_prev = _sprite_index;
+         _sprite_index = static_cast<int32_t>(_state_time_s);
+      }
+
+      State _state{State::Retracted};
+      sf::Time _wait_time;
+      sf::Time _wait_offset;
+      float _state_time_s{0.0f};
+      sf::Sprite _sprite;
+      int32_t _sprite_index{0};
+      int32_t _sprite_index_prev{-1};
+      sf::IntRect _collision_rect_relative;
+      sf::IntRect _collision_rect_absolute;
    };
 
    void setupTransform();
@@ -61,46 +88,30 @@ private:
    void updateBoundingBox();
    void updateSprites();
    void updatePosition(const sf::Time& dt);
+   void updateStatesInterval(const sf::Time& dt);
 
    //     +---+
    //     | 0 |
    // +---+---+---+
-   // | 3 | 4 | 1 |
+   // | 3 |   | 1 |
    // +---+---+---+
    //     | 2 |
    //     +---+
 
-   std::array<sf::Sprite, 5> _sprites;
-
-   std::array<int32_t, 5> _sprite_indices = {0, 0, 0, 0, 0};
-   std::array<float, 5> _state_times = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
-
-   // std::array<sf::Vector2i, 5> _offsets =
-   //    {sf::Vector2i{1, 0}, sf::Vector2i{0, 1}, sf::Vector2i{1, 1}, sf::Vector2i{1, 2}, sf::Vector2i{2, 1}};
-
-   std::array<sf::IntRect, 4> _collision_rects = {
-      sf::IntRect{1 * PIXELS_PER_TILE, 0 * PIXELS_PER_TILE, PIXELS_PER_TILE, PIXELS_PER_TILE},
-      sf::IntRect{0 * PIXELS_PER_TILE, 1 * PIXELS_PER_TILE, PIXELS_PER_TILE, PIXELS_PER_TILE},
-      sf::IntRect{1 * PIXELS_PER_TILE, 2 * PIXELS_PER_TILE, PIXELS_PER_TILE, PIXELS_PER_TILE},
-      sf::IntRect{2 * PIXELS_PER_TILE, 1 * PIXELS_PER_TILE, PIXELS_PER_TILE, PIXELS_PER_TILE}
-   };
-
    sf::Vector2f _pixel_positions;
    sf::FloatRect _rect;
    std::vector<sf::Vector2f> _pixel_paths;
-
    b2Body* _body = nullptr;
    std::vector<b2Vec2> _path;
    PathInterpolation<b2Vec2> _interpolation;
    float _lever_lag = 1.0f;  // maybe make them switchable as well?
    float _velocity = 1.0f;
-
    sf::Time _time_on;
    sf::Time _time_off;
-   std::array<State, 4> _states{State::Extracted, State::Extracted, State::Extracted, State::Extracted};
-   std::array<sf::Time, 4> _times;
-
+   std::array<Spike, 4> _spikes;
+   sf::Sprite _center_sprite;
+   float _center_sprite_time_s{0.0f};
+   int32_t _center_sprite_index{0};
    Mode _mode{Mode::Interval};
-
    std::shared_ptr<sf::Texture> _texture;
 };
