296f319a4c36716c7b482911cc0e791fcd41e1b1
Matthias Varnholt <matthias@cercare-medical.com>
2024-09-20 22:45:18 +0200

    added water acceleration and deceleration

--- src/game/physics/physicsconfiguration.cpp
+++ src/game/physics/physicsconfiguration.cpp
@@ -24,10 +24,14 @@ std::string PhysicsConfiguration::serialize()
           {"player_speed_max_run", _player_speed_max_run},
           {"player_speed_max_water", _player_speed_max_water},
           {"player_friction", _player_friction},
+
           {"player_acceleration_ground", _player_acceleration_ground},
           {"player_deceleration_ground", _player_deceleration_ground},
           {"player_acceleration_air", _player_acceleration_air},
           {"player_deceleration_air", _player_deceleration_air},
+          {"player_acceleration_water", _player_acceleration_water},
+          {"player_deceleration_water", _player_deceleration_water},
+
           {"player_max_velocity_horizontal", _player_max_velocity_horizontal},
           {"player_max_velocity_up", _player_max_velocity_up},
           {"player_max_velocity_down", _player_max_velocity_down},
@@ -90,80 +94,75 @@ void PhysicsConfiguration::deserialize(const std::string& data)
    catch (const std::exception& e)
    {
       std::cerr << e.what() << std::endl;
+      return;
    }
 
-   const auto physics_config = config["PhysicsConfiguration"];
+   const auto& physics_config = config["PhysicsConfiguration"];
 
-   _time_step = physics_config["timestep"].get<float>();
-   _gravity = physics_config["gravity"].get<float>();
-
-   _player_speed_max_walk = physics_config["player_speed_max_walk"].get<float>();
-   _player_speed_max_run = physics_config["player_speed_max_run"].get<float>();
-   _player_speed_max_water = physics_config["player_speed_max_water"].get<float>();
-   _player_speed_max_air = physics_config["player_speed_max_air"].get<float>();
-   _player_friction = physics_config["player_friction"].get<float>();
-
-   _player_acceleration_ground = physics_config["player_acceleration_ground"].get<float>();
-   _player_acceleration_air = physics_config["player_acceleration_air"].get<float>();
-   _player_deceleration_ground = physics_config["player_deceleration_ground"].get<float>();
-   _player_deceleration_air = physics_config["player_deceleration_air"].get<float>();
-
-   if (physics_config.find("player_max_velocity_horizontal") != physics_config.end())
+   auto get_value_if_exists = [](const json& j, const std::string& key, auto& var)
    {
-      _player_max_velocity_horizontal = physics_config["player_max_velocity_horizontal"].get<float>();
-   }
+      if ((j.find(key) != j.end()) && !j[key].is_null())
+      {
+         var = j[key].get<std::decay_t<decltype(var)>>();
+      }
+   };
 
-   if (physics_config.find("player_max_velocity_up") != physics_config.end())
-   {
-      _player_max_velocity_up = physics_config["player_max_velocity_up"].get<float>();
-   }
+   get_value_if_exists(physics_config, "timestep", _time_step);
+   get_value_if_exists(physics_config, "gravity", _gravity);
 
-   if (physics_config.find("player_max_velocity_down") != physics_config.end())
-   {
-      _player_max_velocity_down = physics_config["player_max_velocity_down"].get<float>();
-   }
+   get_value_if_exists(physics_config, "player_speed_max_walk", _player_speed_max_walk);
+   get_value_if_exists(physics_config, "player_speed_max_run", _player_speed_max_run);
+   get_value_if_exists(physics_config, "player_speed_max_water", _player_speed_max_water);
+   get_value_if_exists(physics_config, "player_speed_max_air", _player_speed_max_air);
+   get_value_if_exists(physics_config, "player_friction", _player_friction);
 
-   _player_jump_strength = physics_config["player_jump_strength"].get<float>();
-   _player_jump_frame_count = physics_config["player_jump_steps"].get<int32_t>();
+   get_value_if_exists(physics_config, "player_acceleration_ground", _player_acceleration_ground);
+   get_value_if_exists(physics_config, "player_acceleration_air", _player_acceleration_air);
 
-   if (physics_config.find("player_jump_steps_minimum") != physics_config.end())
-   {
-      _player_jump_frame_count_minimum = physics_config["player_jump_steps_minimum"].get<int32_t>();
-   }
+   get_value_if_exists(physics_config, "player_deceleration_ground", _player_deceleration_ground);
+   get_value_if_exists(physics_config, "player_deceleration_air", _player_deceleration_air);
 
-   _player_jump_after_contact_lost_ms = physics_config["player_jump_after_contact_lost_in_ms"].get<int32_t>();
-   _player_jump_buffer_ms = physics_config["player_jump_buffer_in_ms"].get<int32_t>();
-   _player_jump_minimal_duration_ms = physics_config["player_jump_minimal_duration_in_ms"].get<int32_t>();
-   _player_jump_falloff = physics_config["player_jump_falloff"].get<float>();
-   _player_jump_speed_factor = physics_config["player_jump_speed_factor"].get<float>();
+   get_value_if_exists(physics_config, "player_max_velocity_horizontal", _player_max_velocity_horizontal);
+   get_value_if_exists(physics_config, "player_max_velocity_up", _player_max_velocity_up);
+   get_value_if_exists(physics_config, "player_max_velocity_down", _player_max_velocity_down);
 
-   _player_dash_frame_count = physics_config["player_dash_frame_count"].get<int32_t>();
-   _player_dash_multiplier = physics_config["player_dash_multiplier"].get<float>();
-   _player_dash_multiplier_increment_per_frame = physics_config["player_dash_multiplier_increment_per_frame"].get<float>();
-   _player_dash_multiplier_scale_per_frame = physics_config["player_dash_multiplier_scale_per_frame"].get<float>();
-   _player_dash_vector = physics_config["player_dash_vector"].get<float>();
+   get_value_if_exists(physics_config, "player_jump_strength", _player_jump_strength);
+   get_value_if_exists(physics_config, "player_jump_steps", _player_jump_frame_count);
+   get_value_if_exists(physics_config, "player_jump_steps_minimum", _player_jump_frame_count_minimum);
 
-   _player_wall_slide_friction = physics_config["player_wall_slide_friction"].get<float>();
-   _player_wall_jump_frame_count = physics_config["player_wall_jump_frame_count"].get<int32_t>();
-   _player_wall_jump_vector_x = physics_config["player_wall_jump_vector_x"].get<float>();
-   _player_wall_jump_vector_y = physics_config["player_wall_jump_vector_y"].get<float>();
-   _player_wall_jump_multiplier = physics_config["player_wall_jump_multiplier"].get<float>();
-   _player_wall_jump_multiplier_increment_per_frame = physics_config["player_wall_jump_multiplier_increment_per_frame"].get<float>();
-   _player_wall_jump_multiplier_scale_per_frame = physics_config["player_wall_jump_multiplier_scale_per_frame"].get<float>();
-   _player_wall_jump_extra_force = physics_config["player_wall_jump_extra_force"].get<float>();
-   _player_wall_jump_lock_key_duration_ms = physics_config["player_wall_jump_lock_key_duration_ms"].get<int32_t>();
+   get_value_if_exists(physics_config, "player_jump_after_contact_lost_in_ms", _player_jump_after_contact_lost_ms);
+   get_value_if_exists(physics_config, "player_jump_buffer_in_ms", _player_jump_buffer_ms);
+   get_value_if_exists(physics_config, "player_jump_minimal_duration_in_ms", _player_jump_minimal_duration_ms);
+   get_value_if_exists(physics_config, "player_jump_falloff", _player_jump_falloff);
+   get_value_if_exists(physics_config, "player_jump_speed_factor", _player_jump_speed_factor);
 
-   _player_double_jump_factor = physics_config["player_double_jump_factor"].get<float>();
+   get_value_if_exists(physics_config, "player_dash_frame_count", _player_dash_frame_count);
+   get_value_if_exists(physics_config, "player_dash_multiplier", _player_dash_multiplier);
+   get_value_if_exists(physics_config, "player_dash_multiplier_increment_per_frame", _player_dash_multiplier_increment_per_frame);
+   get_value_if_exists(physics_config, "player_dash_multiplier_scale_per_frame", _player_dash_multiplier_scale_per_frame);
+   get_value_if_exists(physics_config, "player_dash_vector", _player_dash_vector);
 
-   _player_hard_landing_damage_enabled = physics_config["player_hard_landing_damage_enabled"].get<bool>();
-   _player_hard_landing_damage_factor = physics_config["player_hard_landing_damage_factor"].get<float>();
-   _player_hard_landing_delay_s = physics_config["player_hard_landing_delay_s"].get<float>();
+   get_value_if_exists(physics_config, "player_wall_slide_friction", _player_wall_slide_friction);
+   get_value_if_exists(physics_config, "player_wall_jump_frame_count", _player_wall_jump_frame_count);
+   get_value_if_exists(physics_config, "player_wall_jump_vector_x", _player_wall_jump_vector_x);
+   get_value_if_exists(physics_config, "player_wall_jump_vector_y", _player_wall_jump_vector_y);
+   get_value_if_exists(physics_config, "player_wall_jump_multiplier", _player_wall_jump_multiplier);
+   get_value_if_exists(physics_config, "player_wall_jump_multiplier_increment_per_frame", _player_wall_jump_multiplier_increment_per_frame);
+   get_value_if_exists(physics_config, "player_wall_jump_multiplier_scale_per_frame", _player_wall_jump_multiplier_scale_per_frame);
+   get_value_if_exists(physics_config, "player_wall_jump_extra_force", _player_wall_jump_extra_force);
+   get_value_if_exists(physics_config, "player_wall_jump_lock_key_duration_ms", _player_wall_jump_lock_key_duration_ms);
 
-   _player_in_water_force_jump_button = physics_config["player_in_water_force_jump_button"].get<float>();
-   _player_in_water_time_to_allow_jump_button_ms = physics_config["player_in_water_time_to_allow_jump_button_ms"].get<int32_t>();
-   _player_in_water_linear_velocity_y_clamp_min = physics_config["player_in_water_linear_velocity_y_clamp_min"].get<float>();
-   _player_in_water_linear_velocity_y_clamp_max = physics_config["player_in_water_linear_velocity_y_clamp_max"].get<float>();
-   _in_water_buoyancy_force = physics_config["in_water_buoyancy_force"].get<float>();
+   get_value_if_exists(physics_config, "player_double_jump_factor", _player_double_jump_factor);
+
+   get_value_if_exists(physics_config, "player_hard_landing_damage_enabled", _player_hard_landing_damage_enabled);
+   get_value_if_exists(physics_config, "player_hard_landing_damage_factor", _player_hard_landing_damage_factor);
+   get_value_if_exists(physics_config, "player_hard_landing_delay_s", _player_hard_landing_delay_s);
+
+   get_value_if_exists(physics_config, "player_in_water_force_jump_button", _player_in_water_force_jump_button);
+   get_value_if_exists(physics_config, "player_in_water_time_to_allow_jump_button_ms", _player_in_water_time_to_allow_jump_button_ms);
+   get_value_if_exists(physics_config, "player_in_water_linear_velocity_y_clamp_min", _player_in_water_linear_velocity_y_clamp_min);
+   get_value_if_exists(physics_config, "player_in_water_linear_velocity_y_clamp_max", _player_in_water_linear_velocity_y_clamp_max);
+   get_value_if_exists(physics_config, "in_water_buoyancy_force", _in_water_buoyancy_force);
 }
 
 void PhysicsConfiguration::deserializeFromFile(const std::string& filename)
