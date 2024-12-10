5c3d4f9c0e7ea6724236b4e0955d3d0b68e19028
Matthias Varnholt <matthias.varnholt@gmail.com>
2024-12-10 21:41:10 +0100

    code cleanup

--- src/game/controller/gamecontrollerdata.cpp
+++ src/game/controller/gamecontrollerdata.cpp
@@ -1,25 +1,21 @@
 #include "gamecontrollerdata.h"
 
-//-----------------------------------------------------------------------------
 GameControllerData& GameControllerData::getInstance()
 {
    static GameControllerData __instance;
    return __instance;
 }
 
-//-----------------------------------------------------------------------------
 const GameControllerInfo& GameControllerData::getJoystickInfo() const
 {
    return _joystick_info;
 }
 
-//-----------------------------------------------------------------------------
 void GameControllerData::setJoystickInfo(const GameControllerInfo& joystickInfo)
 {
    _joystick_info = joystickInfo;
 }
 
-//-----------------------------------------------------------------------------
 bool GameControllerData::isControllerUsed() const
 {
    return !_joystick_info.getAxisValues().empty();
