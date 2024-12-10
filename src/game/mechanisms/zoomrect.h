8047d07c641cb6114d770b0d3e6307111f8a2f3d
Matthias Varnholt <matthias.varnholt@gmail.com>
2024-12-10 21:41:44 +0100

    add zoomrect progress

---
+++ src/game/mechanisms/zoomrect.h
@@ -0,0 +1,35 @@
+#ifndef ZOOMRECT_H
+#define ZOOMRECT_H
+
+#include "game/io/gamedeserializedata.h"
+#include "game/level/gamenode.h"
+#include "game/mechanisms/gamemechanism.h"
+
+class ZoomRect : public GameMechanism, public GameNode
+{
+public:
+   enum class Normalization
+   {
+      None,
+      Linear
+   };
+
+   struct ZoomFactor
+   {
+      float _radius{1.0f};
+      float _factor{1.0f};
+   };
+
+   ZoomRect(GameNode* parent);
+   void update(const sf::Time& dt) override;
+   void setup(const GameDeserializeData& data);
+
+private:
+   Normalization _normalization{Normalization::Linear};
+   sf::FloatRect _rect_px;
+   sf::Vector2f _center_px;
+   float _radius_px{0.0};
+   std::vector<ZoomFactor> _zoom_factors;
+};
+
+#endif  // ZOOMRECT_H
