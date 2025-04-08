13cf021f3d8553eb27a10b42728cc47a18906a3c
Matthias Varnholt <matthias.varnholt@gmail.com>
2025-04-08 22:09:10 +0200

    added some music just to get the idea

---
+++ data/music/convert_to_ogg.md
@@ -0,0 +1,1 @@
+c:\util\imagemagick\ffmpeg.exe -i input.mp3 -c:a libvorbis -qscale:a 5 output.ogg
--- src/game/game.cpp
+++ src/game/game.cpp
@@ -276,10 +276,32 @@ void Game::initializeController()
    gji.initialize();
 }
 
+void Game::playMenuMusic()
+{
+   Audio::getInstance().getMusicPlayer().queueTrack(
+      {.filename = "data/music/menu_test_track_muffler_callisto.ogg",
+       .transition = Audio::TransitionType::Crossfade,
+       .duration = std::chrono::milliseconds(1000),
+       .post_action = Audio::PostPlaybackAction::Loop}
+   );
+}
+
+void Game::playLevelMusic()
+{
+   Audio::getInstance().getMusicPlayer().queueTrack(
+      {.filename = "data/music/level_test_track_muffler_awakening.ogg",
+       .transition = Audio::TransitionType::Crossfade,
+       .duration = std::chrono::milliseconds(1000),
+       .post_action = Audio::PostPlaybackAction::Loop}
+   );
+}
+
 void Game::showMainMenu()
 {
    Menu::getInstance()->show(Menu::MenuType::Main);
    GameState::getInstance().enqueuePause();
+
+   playMenuMusic();
 }
 
 void Game::showPauseMenu()
@@ -308,6 +330,7 @@ void Game::showPauseMenu()
       Menu::getInstance()->show(Menu::MenuType::Pause);
       GameState::getInstance().enqueuePause();
       _audio_callback(GameAudio::SoundEffect::GameStatePause);
+      playMenuMusic();
    }
 }
 
@@ -349,6 +372,8 @@ void Game::loadLevel(LoadingMode loading_mode)
          Log::Info() << "level loading finished: " << level_item._level_name;
 
          _level_loading_finished = true;
+
+         playLevelMusic();
 
          // before synchronizing the camera with the player position, the camera needs to know its room limitations
          _level->syncRoom();
