26829235439ff497469c581ac41f11adc07a4ca3
Matthias Varnholt <matthias.varnholt@gmail.com>
2025-05-20 15:33:30 +0200

    fixed parallax maps. maybe.

--- src/game/layers/parallaxlayer.cpp
+++ src/game/layers/parallaxlayer.cpp
@@ -5,16 +5,16 @@
 
 void ParallaxLayer::updateView(float level_view_x, float level_view_y, float view_width, float view_height)
 {
-   _view = sf::View(
-      {level_view_x * _settings._factor.x + _settings._error.x, level_view_y * _settings._factor.y + _settings._error.y},
-      {view_width, view_height}
-   );
+   const auto x_px = level_view_x * _settings._factor.x + _settings._error.x;
+   const auto y_px = level_view_y * _settings._factor.y + _settings._error.y;
+
+   _view = sf::View(sf::FloatRect{{x_px, y_px}, {view_width, view_height}});
 }
 
 void ParallaxLayer::resetView(float view_width, float view_height)
 {
-   _view = sf::View({0.0f, 0.0f}, {view_width, view_height});
-   _view.setViewport(sf::FloatRect({0.0f, 0.0f}, {1.0f, 1.0f}));
+   _view = sf::View{sf::FloatRect{{0.0f, 0.0f}, {view_width, view_height}}};
+   _view.setViewport(sf::FloatRect{{0.0f, 0.0f}, {1.0f, 1.0f}});
 }
 
 std::unique_ptr<ParallaxLayer> ParallaxLayer::deserialize(const std::shared_ptr<TmxLayer>& layer, const std::shared_ptr<TileMap>& tile_map)
