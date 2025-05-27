349025dd6c97d52d1efddfa363f61aca2b54ef8a
Matthias Varnholt <matthias.varnholt@gmail.com>
2025-05-27 22:59:58 +0200

    add deploy script for release builds

---
+++ deploy_rel.bat
@@ -0,0 +1,11 @@
+IF NOT EXIST ..\build mkdir ..\build
+IF NOT EXIST ..\build\data mkdir ..\build\data
+IF NOT EXIST ..\build\tools mkdir ..\build\tools
+
+xcopy /E /Y data ..\build\data
+xcopy /E /Y tools ..\build\tools
+xcopy /E /Y tools\level_selector\level_selector.exe ..\build
+xcopy /Y build_rel\install\bin\*.dll ..\build
+xcopy /Y build_rel\deceptus.exe ..\build
+
+IF EXIST deceptus.pdb xcopy /C /Y deceptus.pdb ..\build
