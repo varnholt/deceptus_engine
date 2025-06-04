aec8c439bcf695de58320448d11e92973e2d6749
Matthias Varnholt <matthias.varnholt@gmail.com>
2025-06-04 21:06:30 +0200

    code cleanup

--- src/game/level/gamenode.h
+++ src/game/level/gamenode.h
@@ -17,10 +17,9 @@ public:
    void setObjectId(const std::string& object_id);
 
    std::string getClassName() const;
-
-protected:
    void setClassName(const std::string& name);
 
+protected:
    std::string _class_name;
    std::string _object_id;
    std::vector<GameNode*> _children;
