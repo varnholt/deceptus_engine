7985bb3aac5fff5668497019badecfcdd11a927f
Matthias Varnholt <matthias.varnholt@gmail.com>
2025-06-28 23:47:06 +0200

    add imgui to testbed

--- lab/render_testbed/main.cpp
+++ lab/render_testbed/main.cpp
@@ -1,3 +1,5 @@
+#include <imgui-SFML.h>
+#include <imgui.h>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
@@ -11,6 +13,8 @@ int main()
    sf::VideoMode mode({1280, 720});
    window.create(mode, title);
 
+   ImGui::SFML::Init(window);  // ImGui init
+
    TestMechanism mechanism;
    sf::Clock clock;
 
@@ -18,6 +22,7 @@ int main()
    {
       while (auto event = window.pollEvent())
       {
+         ImGui::SFML::ProcessEvent(window, *event);
          if (event->is<sf::Event::Closed>())
          {
             window.close();
@@ -26,15 +31,26 @@ int main()
          {
             window.setView(sf::View(sf::FloatRect({0.f, 0.f}, {static_cast<float>(resize->size.x), static_cast<float>(resize->size.y)})));
          }
+         else if (auto* key = event->getIf<sf::Event::KeyPressed>())
+         {
+            if (key->code == sf::Keyboard::Key::Space)
+            {
+               mechanism.chooseNextState();
+            }
+         }
       }
 
       sf::Time dt = clock.restart();
+      ImGui::SFML::Update(window, dt);  // ImGui update
+
       mechanism.update(dt);
 
       window.clear();
       mechanism.draw(window, window);
+      ImGui::SFML::Render(window);  // ImGui render
       window.display();
    }
 
+   ImGui::SFML::Shutdown();  // ImGui shutdown
    return 0;
 }
