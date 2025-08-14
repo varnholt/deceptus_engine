27add1e2c2d4c93d986ec2cd2c46487d0d0a5a83
Matthias Varnholt <matthias@cercare-medical.com>
2025-08-14 18:37:52 +0200

    delete old levels

--- data/level-demo/wood.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="wood" tilewidth="24" tileheight="24" tilecount="400" columns="20">
- <image source="tilesheets/wood.png" width="480" height="480"/>
-</tileset>
