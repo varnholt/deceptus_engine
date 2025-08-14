3fad31e8e56b2455014a72978c6e96ded039ecc0
Matthias Varnholt <matthias.varnholt@gmail.com>
2025-08-14 18:37:52 +0200

    delete old levels

--- data/level-malte/generate_ao.bat
+++
@@ -1,3 +0,0 @@
-..\..\tools\tmx_rasterizer\tmxrasterizer.exe --show-layer level malte.tmx malte.png
-..\..\tools\generate_ao\generate_ao.exe malte.png
-..\..\tools\pack_texture\packtexture.exe --input=malte_ao.png --size=64
