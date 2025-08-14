8202a7113ac1af0b2594b78f9cad8684a5aa559c
Matthias Varnholt <matthias.varnholt@gmail.com>
2025-08-14 21:32:18 +0200

    fix build and configs

--- deploy_appveyor.bat
+++ deploy_appveyor.bat
@@ -6,7 +6,7 @@ xcopy /E /Y data artifacts\data
 xcopy /E /Y tools artifacts\tools
 xcopy /E /Y tools\level_selector\level_selector.exe artifacts
 xcopy /Y build\install\bin\* artifacts
-xcopy /E /Y tools\deceptus_updater\deceptus_updater.exe artifacts
-xcopy /E /Y tools\deceptus_updater\files.json artifacts
+xcopy /E /Y tools\deceptus-updater\deceptus_updater.exe artifacts
+xcopy /E /Y tools\deceptus-updater\files.json artifacts
 
 IF EXIST build\Release\deceptus.pdb xcopy /C /Y build\Release\deceptus.pdb artifacts
