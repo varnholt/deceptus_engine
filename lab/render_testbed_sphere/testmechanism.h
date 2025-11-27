85b7292ab49a6bdaeaf86e6e8f1c3c8b0b66f9dc
Matthias Varnholt <matthias@cercare-medical.com>
2025-11-27 21:37:08 +0100

    load starmap

--- lab/render_testbed_sphere/testmechanism.h
+++ lab/render_testbed_sphere/testmechanism.h
@@ -5,6 +5,7 @@
 
 // Include OpenGL and related headers from shmup project
 #include "vbos/vbosphere.h"
+#include "vbos/vbomesh.h"
 #include "game/camera.h"
 #include "opengl/glslprogram.h"
 
@@ -12,7 +13,7 @@ class TestMechanism
 {
 public:
    TestMechanism();
-   virtual ~TestMechanism() = default;
+   virtual ~TestMechanism();
    virtual void draw(sf::RenderTarget& target, sf::RenderTarget& normal);
    virtual void update(const sf::Time& dt);
    void resize(int width, int height);
@@ -23,11 +24,16 @@ private:
 
    // 3D components
    std::unique_ptr<VBOSphere> _sphere;
+   std::unique_ptr<VBOMesh> _starmapMesh;
    float _rotationSpeed{0.5f};  // Rotation speed in radians per second
    float _currentRotation{0.0f};
+   float _starmapRotation{0.0f};
 
    // Camera
    Camera* _camera;
+
+   // Texture ID for starmap
+   GLuint _starmapTextureId{0};
 
    // For proper cleanup of OpenGL resources
    bool _initialized{false};
