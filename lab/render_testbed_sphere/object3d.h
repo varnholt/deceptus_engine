162e00b73ce50dc49c52a5f4be9f2f9b1192756d
Matthias Varnholt <matthias.varnholt@gmail.com>
2025-11-27 21:55:02 +0100

    refactored objects to separate classes

---
+++ lab/render_testbed_sphere/object3d.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include "opengl/glslprogram.h"
+#include <memory>
+
+class Object3D
+{
+public:
+    Object3D() = default;
+    virtual ~Object3D() = default;
+
+    virtual void update(float deltaTime) = 0;
+    virtual void render(const std::shared_ptr<GLSLProgram>& shader,
+                       const glm::mat4& view_matrix,
+                       const glm::mat4& projection_matrix) = 0;
+    virtual void setPosition(const glm::vec3& position) { _position = position; }
+    virtual glm::vec3 getPosition() const { return _position; }
+
+    virtual void setRotation(const glm::vec3& rotation) { _rotation = rotation; }
+    virtual glm::vec3 getRotation() const { return _rotation; }
+
+    virtual void setScale(const glm::vec3& scale) { _scale = scale; }
+    virtual glm::vec3 getScale() const { return _scale; }
+
+protected:
+    glm::vec3 _position{0.0f, 0.0f, 0.0f};
+    glm::vec3 _rotation{0.0f, 0.0f, 0.0f};
+    glm::vec3 _scale{1.0f, 1.0f, 1.0f};
+};
--- lab/render_testbed_sphere/testmechanism.h
+++ lab/render_testbed_sphere/testmechanism.h
@@ -2,12 +2,13 @@
 
 #include <SFML/Graphics.hpp>
 #include <memory>
+#include <vector>
 
 // Include OpenGL and related headers from shmup project
-#include "vbos/vbosphere.h"
-#include "vbos/vbomesh.h"
 #include "game/camera.h"
 #include "opengl/glslprogram.h"
+#include "sphereobject.h"
+#include "texturedobject.h"
 
 class TestMechanism
 {
@@ -22,18 +23,11 @@ public:
 private:
    void load();
 
-   // 3D components
-   std::unique_ptr<VBOSphere> _sphere;
-   std::unique_ptr<VBOMesh> _starmapMesh;
-   float _rotationSpeed{0.5f};  // Rotation speed in radians per second
-   float _currentRotation{0.0f};
-   float _starmapRotation{0.0f};
+   // 3D objects
+   std::vector<std::unique_ptr<Object3D>> _objects;
 
    // Camera
    Camera* _camera;
-
-   // Texture ID for starmap
-   GLuint _starmapTextureId{0};
 
    // For proper cleanup of OpenGL resources
    bool _initialized{false};
