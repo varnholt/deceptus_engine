b2f4f726877a4bca4fdd7e76a09bb611d32df002
Matthias Varnholt <matthias@cercare-medical.com>
2025-12-08 21:36:04 +0100

    code cleanup

--- src/game/player/playerbend.h
+++ src/game/player/playerbend.h
@@ -9,11 +9,30 @@ struct PlayerBend
 
    bool _bending_down = false;
    bool _was_bending_down = false;
+
    bool _crouching = false;
    bool _was_crouching = false;
 
    HighResTimePoint _timepoint_bend_down_start;
    HighResTimePoint _timepoint_bend_down_end;
+
+   void resetBendingDown()
+   {
+      _bending_down = false;
+      _was_bending_down = false;
+   }
+
+   void setBendingDownEnabled(bool bending_down)
+   {
+      _was_bending_down = _bending_down;
+      _bending_down = bending_down;
+   }
+
+   void setCrouchingEnabled(bool crouching)
+   {
+      _was_crouching = _crouching;
+      _crouching = crouching;
+   }
 
    bool isCrouching() const
    {
