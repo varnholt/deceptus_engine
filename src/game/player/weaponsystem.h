566285f376ebc563f862683055eb365247552ae4
Matthias Varnholt <46523709+varnholt@users.noreply.github.com>
2026-03-19 22:17:52 +0100

    revert most of weapon system

--- src/game/player/weaponsystem.h
+++ src/game/player/weaponsystem.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <memory>
-#include <vector>
 #include <array>
+#include <memory>
 #include <string>
+#include <vector>
 
 #include "json/json.hpp"
 
@@ -13,10 +13,6 @@ struct b2Body;
 struct WeaponSystem
 {
    WeaponSystem() = default;
-
-   void onInventoryItemAdded(const std::string& item_name, b2Body* player_body);
-   void onInventoryItemRemoved(const std::string& item_name);
-   void syncWithInventory(const std::array<std::string, 2>& slots, b2Body* player_body);
 
    std::shared_ptr<Weapon> _selected;
    std::vector<std::shared_ptr<Weapon>> _weapons;
